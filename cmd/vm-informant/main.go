3bff24edf98f20fb9aeb2f2b35c6a5f8d508f463
sharnoff <sharnoff@neon.tech>
2023-01-30 14:27:22 -0800

    informant: add comment about null string arg default

--- cmd/vm-informant/main.go
+++ cmd/vm-informant/main.go
@@ -16,6 +16,9 @@ func main() {
 	klog.Infof("buildInfo.GitInfo:   %s", buildInfo.GitInfo)
 	klog.Infof("buildInfo.GoVersion: %s", buildInfo.GoVersion)
 
+	// cgroup names *could* be any valid path fragment. We can distinguish --cgroup="" and not
+	// setting the cgroup by having a default value of a null byte, which won't be a valid path
+	// anyways.
 	nullString := "\x00"
 	var cgroupName string
 	flag.StringVar(&cgroupName, "cgroup", nullString, "Sets the cgroup to monitor (optional)")
