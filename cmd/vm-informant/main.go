504b80a624af6b8bcaecfaefda68db3b173faf58
sharnoff <sharnoff@neon.tech>
2023-02-05 19:28:55 -0800

    rework cgroup name flag + comment

--- cmd/vm-informant/main.go
+++ cmd/vm-informant/main.go
@@ -16,18 +16,20 @@ func main() {
 	klog.Infof("buildInfo.GitInfo:   %s", buildInfo.GitInfo)
 	klog.Infof("buildInfo.GoVersion: %s", buildInfo.GoVersion)
 
-	// cgroup names *could* be any valid path fragment. We can distinguish --cgroup="" and not
-	// setting the cgroup by having a default value of a null byte, which won't be a valid path
-	// anyways.
-	nullString := "\x00"
+	// Realistically, we want to be able to distinguish between --cgroup="" and absence of the flag,
+	// if only to be able to provide better errors on invalid cgroup names.
+	//
+	// Because cgroup names *could* be any valid path fragment, we can include a null byte in the
+	// string to distinguish this, because null bytes aren't valid in paths.
+	noCgroup := "invalid\x00CgroupName"
 	var cgroupName string
-	flag.StringVar(&cgroupName, "cgroup", nullString, "Sets the cgroup to monitor (optional)")
+	flag.StringVar(&cgroupName, "cgroup", noCgroup, "Sets the cgroup to monitor (optional)")
 
 	flag.Parse()
 
 	var stateOpts []informant.NewStateOpts
 
-	if cgroupName != nullString {
+	if cgroupName != noCgroup {
 		cgroupConfig := informant.DefaultCgroupConfig
 		klog.Infof("Selected cgroup %q, starting handler with config %+v", cgroupName, cgroupConfig)
 		cgroup, err := informant.NewCgroupManager(cgroupName)
