97d205ba91402f67bbd7885e23fc70758f2d6203
tycho garen <garen@tychoish.com>
2023-02-21 18:14:28 -0500

    informant: handle signals

--- cmd/vm-informant/main.go
+++ cmd/vm-informant/main.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/containerd/cgroups/v3/cgroup2"
@@ -19,6 +21,9 @@ import (
 )
 
 func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM)
+	defer cancel()
+
 	buildInfo := util.GetBuildInfo()
 	klog.Infof("buildInfo.GitInfo:   %s", buildInfo.GitInfo)
 	klog.Infof("buildInfo.GoVersion: %s", buildInfo.GoVersion)
@@ -97,54 +102,103 @@ func main() {
 	}
 }
 
+const minWaitDuration = 5 * time.Second
+
 // runRestartOnFailure repeatedly calls this binary with the same flags, but with 'auto-restart'
 // removed.
 //
 // We execute ourselves as a subprocess so that it's possible to appropriately cleanup after
 // termination by various signals (or an unhandled panic!). This is worthwhile because we *really*
 // don't want to leave the cgroup frozen while waiting to restart.
-func runRestartOnFailure(args []string, cleanupHooks []func()) {
+func runRestartOnFailure(ctx context.Context, args []string, cleanupHooks []func()) {
 	selfPath := os.Args[0]
-
-	minWaitDuration := time.Second * 5
+	timer := time.NewTimer(0)
+	defer timer.Stop()
 
 	for {
 		startTime := time.Now()
-
-		cmd := exec.CommandContext(context.TODO(), selfPath, args...)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		err := cmd.Run()
-
+		sig := make(chan struct{})
 		var exitMode string
 
-		if err != nil {
-			// lint note: the linter's worried about wrapped errors being incorrect with switch, but
-			// this is cleaner than the alternative (via errors.As) and it's still correct because
-			// exec.Command.Run() explicitly mentions ExitError.
-			switch err.(type) { //nolint:errorlint // see above.
-			case *exec.ExitError:
-				exitMode = "failed"
-				klog.Errorf("vm-informant exited with: %v", err)
-			default:
-				exitMode = "failed to start"
-				klog.Errorf("error running vm-informant: %v", err)
+		func() {
+			pctx, pcancel := context.WithCancel(context.Background())
+			defer pcancel()
+
+			cmd := exec.Command(selfPath, args...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			go func() {
+				defer close(sig)
+				select {
+				case <-pctx.Done():
+					return
+				case <-ctx.Done():
+					if cmd.Process == nil || cmd.ProcessState == nil {
+						return
+					}
+					if cmd.ProcessState.ExitCode() >= 0 {
+						return
+					}
+					var pid int
+					if cmd.Process != nil {
+						pid = cmd.Process.Pid
+					}
+					if err := cmd.Process.Signal(syscall.SIGTERM); err != nil {
+						klog.Infof("could not signal informant process %d: %v", pid, err)
+					}
+				}
+			}()
+
+			err := cmd.Run()
+
+			if err != nil {
+				// lint note: the linter's worried about wrapped errors being incorrect with switch, but
+				// this is cleaner than the alternative (via errors.As) and it's still correct because
+				// exec.Command.Run() explicitly mentions ExitError.
+				switch err.(type) { //nolint:errorlint // see above.
+				case *exec.ExitError:
+					exitMode = "failed"
+					klog.Errorf("vm-informant exited with: %v", err)
+				default:
+					exitMode = "failed to start"
+					klog.Errorf("error running vm-informant: %v", err)
+				}
+			} else {
+				exitMode = ""
+				klog.Warningf("vm-informant exited without error. This should not happen.")
 			}
-		} else {
-			exitMode = ""
-			klog.Warningf("vm-informant exited without error. This should not happen.")
-		}
 
-		for _, h := range cleanupHooks {
-			h()
-		}
+			pcancel()
+			for _, h := range cleanupHooks {
+				h()
+			}
+		}()
 
-		dur := time.Since(startTime)
-		if dur < minWaitDuration {
-			klog.Infof("vm-informant %s. respecting minimum wait of %s", exitMode, minWaitDuration)
-			time.Sleep(minWaitDuration - dur)
-		} else {
+		select {
+		case <-ctx.Done():
+			klog.Infof("vm0informant: received signal")
+			return
+		case <-sig:
+			dur := time.Since(startTime)
+			if dur < minWaitDuration {
+				// drain the timer before resetting it:
+				if !timer.Stop() {
+					<-timer.C
+				}
+				timer.Reset(minWaitDuration - dur)
+
+				klog.Infof("vm-informant %s. respecting minimum wait of %s", exitMode, minWaitDuration)
+				select {
+				case <-ctx.Done():
+					klog.Infof("vm0informant: received signal")
+					return
+				case <-timer.C:
+					continue
+				}
+			}
+
 			klog.Infof("vm-informant restarting immediately")
+			continue
 		}
 	}
 }
