neondatabase/autoscaling

commit bd9595ecf07c70a642e54cf7043446e2fcbe5a5b

agent: plumb contexts through (#59)

go.mod
@@ -37,7 +37,7 @@ require (
 	github.com/elastic/go-sysinfo v1.9.0
 	github.com/google/uuid v1.3.0
 	github.com/neondatabase/neonvm v0.4.6
-	github.com/tychoish/fun v0.3.3
+	github.com/tychoish/fun v0.7.0
 	golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9
 	k8s.io/api v0.23.15
 	k8s.io/apimachinery v0.23.15

go.sum
@@ -653,8 +653,8 @@ github.com/syndtr/gocapability v0.0.0-20200815063812-42c35b437635/go.mod h1:hkRG
 github.com/tmc/grpc-websocket-proxy v0.0.0-20190109142713-0ad062ec5ee5/go.mod h1:ncp9v5uamzpCO7NfCPTXjqaC+bZgJeR0sMTm6dMHP7U=
 github.com/tmc/grpc-websocket-proxy v0.0.0-20201229170055-e5319fda7802 h1:uruHq4dN7GR16kFc5fp3d1RIYzJW5onx8Ybykw2YQFA=
 github.com/tmc/grpc-websocket-proxy v0.0.0-20201229170055-e5319fda7802/go.mod h1:ncp9v5uamzpCO7NfCPTXjqaC+bZgJeR0sMTm6dMHP7U=
-github.com/tychoish/fun v0.3.3 h1:oyptuZ9SL8vzPamERbZA2PnG7VMFyMBimVlJXn9rgas=
-github.com/tychoish/fun v0.3.3/go.mod h1:84A+BwGecz23UotmbB4mtvVS5ZcsZpspecduxpwF/XM=
+github.com/tychoish/fun v0.7.0 h1:7bT+TGFBZfhqfVjjyqhj7ze2GelUH+hZL3FYnL7x/dQ=
+github.com/tychoish/fun v0.7.0/go.mod h1:84A+BwGecz23UotmbB4mtvVS5ZcsZpspecduxpwF/XM=
 github.com/ugorji/go v1.1.4/go.mod h1:uQMGLiO92mf5W77hV/PUCpI3pbzQx3CRekS0kk+RGrc=
 github.com/urfave/cli v1.22.1/go.mod h1:Gos4lmkARVdJ6EkW0WaNv/tZAAMe9V7XWyB60NtXRu0=
 github.com/urfave/cli v1.22.2/go.mod h1:Gos4lmkARVdJ6EkW0WaNv/tZAAMe9V7XWyB60NtXRu0=

cmd/autoscaler-agent/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"context"
+	"os/signal"
+	"syscall"
+
+	"github.com/tychoish/fun/srv"
+
 	"k8s.io/client-go/kubernetes"
 	scheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
@@ -48,8 +54,12 @@ func main() {
 		VMClient:   vmClient,
 	}
 
-	err = runner.Run()
-	if err != nil {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM)
+	defer cancel()
+	ctx = srv.SetShutdown(ctx)
+	ctx = srv.SetBaseContext(ctx)
+
+	if err = runner.Run(ctx); err != nil {
 		klog.Fatalf("Main loop failed: %s", err)
 	}
 	klog.Info("Main loop returned without issue. Exiting.")

pkg/agent/entrypoint.go
@@ -22,9 +22,7 @@ type MainRunner struct {
 	VMClient   *vmclient.Clientset
 }
 
-func (r MainRunner) Run() error {
-	ctx := context.Background()
-
+func (r MainRunner) Run(ctx context.Context) error {
 	podEvents := make(chan podEvent)
 
 	buildInfo := util.GetBuildInfo()
@@ -87,7 +85,7 @@ func (r MainRunner) Run() error {
 			globalState.Stop()
 			return nil
 		case event := <-podEvents:
-			globalState.handleEvent(event)
+			globalState.handleEvent(ctx, event)
 		}
 	}
 }

pkg/agent/globalstate.go
@@ -57,7 +57,7 @@ func (s *agentState) Stop() {
 	}
 }
 
-func (s *agentState) handleEvent(event podEvent) {
+func (s *agentState) handleEvent(ctx context.Context, event podEvent) {
 	klog.Infof("Handling pod event %+v", event)
 
 	state, hasPod := s.pods[event.podName]
@@ -77,7 +77,7 @@ func (s *agentState) handleEvent(event podEvent) {
 			return
 		}
 
-		runnerCtx, cancelRunnerContext := context.WithCancel(context.TODO())
+		runnerCtx, cancelRunnerContext := context.WithCancel(ctx)
 
 		status := &podStatus{
 			lock:     sync.Mutex{},

pkg/agent/informant.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/tychoish/fun/srv"
 
 	"github.com/neondatabase/autoscaling/pkg/api"
 	"github.com/neondatabase/autoscaling/pkg/util"
@@ -199,16 +200,26 @@ func NewInformantServer(
 
 		// we need to spawn these in separate threads so the caller doesn't block while holding
 		// runner.lock
-		runner.spawnBackgroundWorker(context.TODO(), shutdownName, func(c context.Context) {
-			if err := httpServer.Shutdown(c); err != nil {
+		runner.spawnBackgroundWorker(srv.GetBaseContext(ctx), shutdownName, func(context.Context) {
+			// we want shutdown to (potentially) live longer than the request which
+			// made it, but having a timeout is still good.
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+
+			if err := httpServer.Shutdown(ctx); err != nil {
 				runner.logger.Warningf("Error shutting down InformantServer: %s", err)
 			}
 		})
 		if server.madeContact {
 			// only unregister the server if we could have plausibly contacted the informant
 			unregisterName := fmt.Sprintf("InformantServer unregister (%s)", server.desc.AgentID)
-			runner.spawnBackgroundWorker(context.TODO(), unregisterName, func(c context.Context) {
-				if err := server.unregisterFromInformant(c); err != nil {
+			runner.spawnBackgroundWorker(srv.GetBaseContext(ctx), unregisterName, func(context.Context) {
+				// we want shutdown to (potentially) live longer than the request which
+				// made it, but having a timeout is still good.
+				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+				defer cancel()
+
+				if err := server.unregisterFromInformant(ctx); err != nil {
 					runner.logger.Warningf("Error unregistering %s: %s", server.desc.AgentID, err)
 				}
 			})
@@ -614,7 +625,7 @@ func (s *InformantServer) informantURL(path string) string {
 // of that context.
 //
 // Returns: response body (if successful), status code, error (if unsuccessful)
-func (s *InformantServer) handleID(body *struct{}) (*api.AgentIdentificationMessage, int, error) {
+func (s *InformantServer) handleID(ctx context.Context, body *struct{}) (*api.AgentIdentificationMessage, int, error) {
 	s.runner.lock.Lock()
 	defer s.runner.lock.Unlock()
 
@@ -634,7 +645,7 @@ func (s *InformantServer) handleID(body *struct{}) (*api.AgentIdentificationMess
 // outside of that context.
 //
 // Returns: response body (if successful), status code, error (if unsuccessful)
-func (s *InformantServer) handleResume(body *api.ResumeAgent) (*api.AgentIdentificationMessage, int, error) {
+func (s *InformantServer) handleResume(ctx context.Context, body *api.ResumeAgent) (*api.AgentIdentificationMessage, int, error) {
 	if body.ExpectedID != s.desc.AgentID {
 		s.runner.logger.Warningf("AgentID %q not found, server has %q", body.ExpectedID, s.desc.AgentID)
 		return nil, 404, fmt.Errorf("AgentID %q not found", body.ExpectedID)
@@ -693,7 +704,7 @@ func (s *InformantServer) handleResume(body *api.ResumeAgent) (*api.AgentIdentif
 // called outside of that context.
 //
 // Returns: response body (if successful), status code, error (if unsuccessful)
-func (s *InformantServer) handleSuspend(body *api.SuspendAgent) (*api.AgentIdentificationMessage, int, error) {
+func (s *InformantServer) handleSuspend(ctx context.Context, body *api.SuspendAgent) (*api.AgentIdentificationMessage, int, error) {
 	if body.ExpectedID != s.desc.AgentID {
 		s.runner.logger.Warningf("AgentID %q not found, server has %q", body.ExpectedID, s.desc.AgentID)
 		return nil, 404, fmt.Errorf("AgentID %q not found", body.ExpectedID)
@@ -747,6 +758,7 @@ func (s *InformantServer) handleSuspend(body *api.SuspendAgent) (*api.AgentIdent
 //
 // Returns: response body (if successful), status code, error (if unsuccessful)
 func (s *InformantServer) handleTryUpscale(
+	ctx context.Context,
 	body *api.MoreResourcesRequest,
 ) (*api.AgentIdentificationMessage, int, error) {
 	if body.ExpectedID != s.desc.AgentID {

pkg/informant/endpoints.go
@@ -3,6 +3,7 @@ package informant
 // This file contains the high-level handlers for various HTTP endpoints
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"sync"
@@ -95,7 +96,7 @@ func WithCgroup(cgm *CgroupManager, config CgroupConfig) NewStateOpts {
 // RegisterAgent registers a new or updated autoscaler-agent
 //
 // Returns: body (if successful), status code, error (if unsuccessful)
-func (s *State) RegisterAgent(info *api.AgentDesc) (*api.InformantDesc, int, error) {
+func (s *State) RegisterAgent(ctx context.Context, info *api.AgentDesc) (*api.InformantDesc, int, error) {
 	protoVersion, status, err := s.agents.RegisterNewAgent(info)
 	if err != nil {
 		return nil, status, err
@@ -115,7 +116,7 @@ func (s *State) RegisterAgent(info *api.AgentDesc) (*api.InformantDesc, int, err
 // amount is ok
 //
 // Returns: body (if successful), status code and error (if unsuccessful)
-func (s *State) TryDownscale(target *api.RawResources) (*api.DownscaleResult, int, error) {
+func (s *State) TryDownscale(ctx context.Context, target *api.RawResources) (*api.DownscaleResult, int, error) {
 	// If we aren't interacting with a cgroup, then we don't need to do anything.
 	if s.cgroup == nil {
 		return &api.DownscaleResult{Ok: true, Status: "No action taken (no cgroup enabled)"}, 200, nil
@@ -173,7 +174,7 @@ func (s *State) TryDownscale(target *api.RawResources) (*api.DownscaleResult, in
 // NotifyUpscale signals that the VM's resource usage has been increased to the new amount
 //
 // Returns: body (if successful), status code and error (if unsuccessful)
-func (s *State) NotifyUpscale(newResources *api.RawResources) (*struct{}, int, error) {
+func (s *State) NotifyUpscale(ctx context.Context, newResources *api.RawResources) (*struct{}, int, error) {
 	// FIXME: we shouldn't just trust what the agent says
 	//
 	// Because of race conditions like in <https://github.com/neondatabase/autoscaling/issues/23>,
@@ -216,7 +217,7 @@ func (s *State) NotifyUpscale(newResources *api.RawResources) (*struct{}, int, e
 // If a different autoscaler-agent is currently registered, this method will do nothing.
 //
 // Returns: body (if successful), status code and error (if unsuccessful)
-func (s *State) UnregisterAgent(info *api.AgentDesc) (*api.UnregisterAgent, int, error) {
+func (s *State) UnregisterAgent(ctx context.Context, info *api.AgentDesc) (*api.UnregisterAgent, int, error) {
 	agent, ok := s.agents.Get(info.AgentID)
 	if !ok {
 		return nil, 404, fmt.Errorf("No agent with ID %q", info.AgentID)

pkg/util/handle.go
@@ -3,6 +3,7 @@ package util
 // Wrapper file for the AddHandler function
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -22,7 +23,7 @@ func AddHandler[T any, R any](
 	endpoint string,
 	method string,
 	reqTypeName string,
-	handle func(*T) (_ *R, statusCode int, _ error),
+	handle func(context.Context, *T) (_ *R, statusCode int, _ error),
 ) {
 	errBadMethod := []byte("request method must be " + method)
 
@@ -44,7 +45,7 @@ func AddHandler[T any, R any](
 
 		klog.Infof("%sReceived request on %s (client = %s) %+v", logPrefix, endpoint, r.RemoteAddr, req)
 
-		resp, status, err := handle(&req)
+		resp, status, err := handle(r.Context(), &req)
 
 		if err == nil && status != http.StatusOK {
 			err = errors.New("HTTP handler error: status != 200 OK, but no error message")

commit 05bf991892916a99bdf7042711abcf69ae8ad4ca

Bump tychoish/fun v0.7.0 -> v0.7.1
Fixes an issue causing autoscaler-agents to crash on startup, introduced
by #59.

go.mod
@@ -37,7 +37,7 @@ require (
 	github.com/elastic/go-sysinfo v1.9.0
 	github.com/google/uuid v1.3.0
 	github.com/neondatabase/neonvm v0.4.6
-	github.com/tychoish/fun v0.7.0
+	github.com/tychoish/fun v0.7.1
 	golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9
 	k8s.io/api v0.23.15
 	k8s.io/apimachinery v0.23.15

go.sum
@@ -653,8 +653,8 @@ github.com/syndtr/gocapability v0.0.0-20200815063812-42c35b437635/go.mod h1:hkRG
 github.com/tmc/grpc-websocket-proxy v0.0.0-20190109142713-0ad062ec5ee5/go.mod h1:ncp9v5uamzpCO7NfCPTXjqaC+bZgJeR0sMTm6dMHP7U=
 github.com/tmc/grpc-websocket-proxy v0.0.0-20201229170055-e5319fda7802 h1:uruHq4dN7GR16kFc5fp3d1RIYzJW5onx8Ybykw2YQFA=
 github.com/tmc/grpc-websocket-proxy v0.0.0-20201229170055-e5319fda7802/go.mod h1:ncp9v5uamzpCO7NfCPTXjqaC+bZgJeR0sMTm6dMHP7U=
-github.com/tychoish/fun v0.7.0 h1:7bT+TGFBZfhqfVjjyqhj7ze2GelUH+hZL3FYnL7x/dQ=
-github.com/tychoish/fun v0.7.0/go.mod h1:84A+BwGecz23UotmbB4mtvVS5ZcsZpspecduxpwF/XM=
+github.com/tychoish/fun v0.7.1 h1:Jup/3zWZ+LYzMMTbg9rj76ZY8zxGPzP/ZLGU4o9hjes=
+github.com/tychoish/fun v0.7.1/go.mod h1:84A+BwGecz23UotmbB4mtvVS5ZcsZpspecduxpwF/XM=
 github.com/ugorji/go v1.1.4/go.mod h1:uQMGLiO92mf5W77hV/PUCpI3pbzQx3CRekS0kk+RGrc=
 github.com/urfave/cli v1.22.1/go.mod h1:Gos4lmkARVdJ6EkW0WaNv/tZAAMe9V7XWyB60NtXRu0=
 github.com/urfave/cli v1.22.2/go.mod h1:Gos4lmkARVdJ6EkW0WaNv/tZAAMe9V7XWyB60NtXRu0=