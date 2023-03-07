ea1fa185f67a039690f4da3ebb9b35a0450ba4a0
sharnoff <sharnoff@neon.tech>
2023-03-07 15:51:09 -0800

    move pcancel out of loop

--- cmd/vm-informant/main.go
+++ cmd/vm-informant/main.go
@@ -115,11 +115,20 @@ func runRestartOnFailure(ctx context.Context, args []string, cleanupHooks []func
 	timer := time.NewTimer(0)
 	defer timer.Stop()
 
+	// CancelFunc for each process's context, for internal signaling.
+	// The defer is out here so we don't leak deferred funcs on restart.
+	var pcancel context.CancelFunc
+	defer func() {
+		if pcancel != nil {
+			pcancel()
+		}
+	}()
+
 	for {
 		startTime := time.Now()
 		func() {
-			pctx, pcancel := context.WithCancel(context.Background())
-			defer pcancel()
+			var pctx context.Context
+			pctx, pcancel = context.WithCancel(context.Background())
 
 			cmd := exec.Command(selfPath, args...)
 			cmd.Stdout = os.Stdout
