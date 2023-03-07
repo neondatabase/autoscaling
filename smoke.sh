2b5cc7c6b80252c9d38bfba4654001c96b1a0cd8
tycho garen <garen@tychoish.com>
2023-03-07 09:30:01 -0500

    fix waiting

--- smoke.sh
+++ smoke.sh
@@ -20,7 +20,7 @@ kubectl apply -f vm-deploy.yaml
 # check a few times for pods to exist
 checks=20
 while [ $checks -ne 0 ]; do
-    POD_NAME=$(kubectl describe pods -l vm.neon.tech/name=postgres14-disk-test --show-events=false | grep -e '^Name:' | sed 's/Name: *p/p/')
+    POD_NAME=$(kubectl describe pods | grep -e "^Name: *postgres14" | sed 's/Name: *[^p]//')
 
     if [ -z "$POD_NAME" ]; then
         sleep .5s
