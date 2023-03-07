13056376aecd3f4314917a3a9b6dc80159c6f571
tycho garen <garen@tychoish.com>
2023-03-06 20:35:57 -0500

    retry

--- smoke.sh
+++ smoke.sh
@@ -17,11 +17,22 @@ scripts/cluster-init.sh
 
 kubectl apply -f vm-deploy.yaml
 
-POD_NAME=$(kubectl describe pods -l vm.neon.tech/name=postgres14-disk-test --show-events=false | grep -e '^Name:' | sed 's/Name:.*p/p/')
+# check a few times for pods to exist
+checks=20
+while [ $checks -ne 0 ]; do
+    POD_NAME=$(kubectl describe pods -l vm.neon.tech/name=postgres14-disk-test --show-events=false | grep -e '^Name:' | sed 's/Name: *p/p/')
+
+    if [ -z "$POD_NAME" ]; then
+        sleep .5s
+        checks=$(($checks - 1))
+    else
+	echo "smoke test succeeded"
+	break
+    fi
+done
 
 if [ -z "$POD_NAME" ]; then
     echo "could not find a running vm pod"
     exit 1
 fi
 
-echo "smoke test succeeded"
