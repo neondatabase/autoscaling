1fc80aa65dafd931c8b9f6f4a72442d64b68ecb7
Andrey Taranik <andrey@cicd.team>
2023-04-07 20:56:00 +0300

    Update neonvm/runner/main.go
    Co-authored-by: sharnoff <sharnoff@neon.tech>

--- neonvm/runner/main.go
+++ neonvm/runner/main.go
@@ -538,7 +538,7 @@ func main() {
 	}
 }
 
-func terminateQemu(qmpPort int32) {
+func terminateQemuOnSigterm(qmpPort int32) {
 	log.Println("watching OS signals")
 	c := make(chan os.Signal, 1) // we need to reserve to buffer size 1, so the notifier are not blocked
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
