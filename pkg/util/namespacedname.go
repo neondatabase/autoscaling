62c08610b5cfc3cd094110e2fb3268f93bcec807
sharnoff <sharnoff@neon.tech>
2023-04-07 14:02:03 -0700

    util: Add NamespacedName instead of api.PodName (#145)
    Defining this functionality in pkg/api means it can't be used in
    pkg/util, because api depends on util. #135 requires it in util.
    
    It's also worth making the name more generic, because it's not just pods
    that we want to handle namespaced names for.

---
+++ pkg/util/namespacedname.go
@@ -0,0 +1,41 @@
+package util
+
+// same as k8s.io/apimachinery/pkg/types/namespacedname.go, but with JSON (de)serialization
+
+import (
+	"fmt"
+)
+
+// FIXME: K8s uses '/' as its separator. We currently use this for backwards-compatibility, but we
+// should change to '/'. That change will involve fixing the many places where we create strings
+// with %s:%s instead of %s/%s as well; those should just use NamespacedName directly.
+const Separator = ':'
+
+// NamespacedName represents a resource name with the namespace it's in.
+//
+// When printed with '%s' or '%v', NamespacedName is rendered as "<namespace>:<name>". Printing with
+// '%+v' or '%#v' renders as it would normally.
+type NamespacedName struct {
+	Namespace string `json:"namespace"`
+	Name      string `json:"name"`
+}
+
+func (n NamespacedName) Format(state fmt.State, verb rune) {
+	switch {
+	case verb == 'v' && state.Flag('+'):
+		// Show fields, e.g. `{Namespace:foo Name:bar}`
+		_, _ = state.Write([]byte(string("{Namespace:")))
+		_, _ = state.Write([]byte(n.Namespace))
+		_, _ = state.Write([]byte(string(" Name:")))
+		_, _ = state.Write([]byte(n.Name))
+		_, _ = state.Write([]byte{'}'})
+	case verb == 'v' && state.Flag('#'):
+		// Go syntax representation, e.g. `util.NamespacedName{Namespace:"foo", Name:"bar"}`
+		_, _ = state.Write([]byte(fmt.Sprintf("util.NamespacedName{Namespace:%q, Name:%q}", n.Namespace, n.Name)))
+	default:
+		// Pretty-printed representation, e.g. `foo:bar`
+		_, _ = state.Write([]byte(n.Namespace))
+		_, _ = state.Write([]byte(string(Separator)))
+		_, _ = state.Write([]byte(n.Name))
+	}
+}
