suryatmodulus/pg-k8s-autoscaling

Require label for autoscaling to be enabled

pkg/agent/globalstate.go
@@ -48,6 +48,7 @@ func podIsOurResponsibility(pod *corev1.Pod, config *Config, nodeName string) bo
 	return pod.Spec.NodeName == nodeName &&
 		pod.Status.PodIP != "" &&
 		pod.Spec.SchedulerName == config.Scheduler.SchedulerName &&
+		api.HasAutoscalingEnabled(pod) &&
 		util.PodReady(pod)
 }
 

pkg/plugin/plugin.go
@@ -83,11 +83,12 @@ func makeAutoscaleEnforcerPlugin(ctx context.Context, obj runtime.Object, h fram
 		return nil, err
 	}
 
-	// Start watching deletion events...
+	// Start watching Pod/VM events...
 	vmDeletions := make(chan api.PodName)
+	vmDisabledScaling := make(chan api.PodName)
 	podDeletions := make(chan api.PodName)
 	klog.Infof("[autoscale-enforcer] Starting pod deletion watcher")
-	if err := p.watchPodDeletions(ctx, vmDeletions, podDeletions); err != nil {
+	if err := p.watchPodEvents(ctx, vmDeletions, vmDisabledScaling, podDeletions); err != nil {
 		return nil, fmt.Errorf("Error starting VM deletion watcher: %w", err)
 	}
 
@@ -107,6 +108,8 @@ func makeAutoscaleEnforcerPlugin(ctx context.Context, obj runtime.Object, h fram
 				return
 			case name := <-vmDeletions:
 				p.handleVMDeletion(name)
+			case name := <-vmDisabledScaling:
+				p.handleVMDisabledScaling(name)
 			case name := <-podDeletions:
 				p.handlePodDeletion(name)
 			}

pkg/plugin/state.go
@@ -633,6 +633,30 @@ func (e *AutoscaleEnforcer) handleVMDeletion(podName api.PodName) {
 	klog.Infof(fmtString, migrating, pod.name, pod.node.name, vCPUVerdict, memVerdict)
 }
 
+func (e *AutoscaleEnforcer) handleVMDisabledScaling(podName api.PodName) {
+	klog.Infof("[autoscale-enforcer] Handling disabling autoscaling for VM pod %v", podName)
+
+	e.state.lock.Lock()
+	defer e.state.lock.Unlock()
+
+	pod, ok := e.state.podMap[podName]
+	if !ok {
+		klog.Warningf("[autoscale-enforcer] disable autoscaling: Cannot find pod %v in podMap", podName)
+		return
+	}
+
+	// Reset buffer to zero:
+	vCPUVerdict := collectResourceTransition(&pod.node.vCPU, &pod.vCPU).
+		handleAutoscalingDisabled()
+	memVerdict := collectResourceTransition(&pod.node.memSlots, &pod.memSlots).
+		handleAutoscalingDisabled()
+
+	fmtString := "[autoscale-enforcer] Disabled autoscaling for VM pod %v in node %s:\n" +
+		"\tvCPU verdict: %s\n" +
+		"\t mem verdict: %s"
+	klog.Infof(fmtString, pod.name, pod.node.name, vCPUVerdict, memVerdict)
+}
+
 func (e *AutoscaleEnforcer) handlePodDeletion(podName api.PodName) {
 	klog.Infof("[autoscale-enforcer] Handling deletion of non-VM pod %v", podName)
 
@@ -864,6 +888,16 @@ func (p *AutoscaleEnforcer) readClusterState(ctx context.Context) error {
 
 			testingOnlyAlwaysMigrate: vmInfo.AlwaysMigrate,
 		}
+
+		// If scaling isn't enabled, then we can be more precise about usage.
+		if !vmInfo.ScalingEnabled {
+			ps.vCPU.buffer = 0
+			ps.vCPU.reserved = vmInfo.Cpu.Use
+
+			ps.memSlots.buffer = 0
+			ps.memSlots.reserved = vmInfo.Mem.Use
+		}
+
 		oldNodeVCPUReserved := ns.vCPU.reserved
 		oldNodeMemReserved := ns.memSlots.reserved
 		oldNodeVCPUBuffer := ns.vCPU.buffer

pkg/plugin/watch.go
@@ -1,5 +1,8 @@
 package plugin
 
+// Implementation of watching Pods for Pod/VM deletions and changes so that a VM's autoscaling is
+// disabled.
+
 // Implementation of watching for VM deletions and VM migration completions, so we can unreserve the
 // associated resources
 
@@ -16,16 +19,44 @@ import (
 	"github.com/neondatabase/autoscaling/pkg/util"
 )
 
-// watchPodDeletions continuously tracks pod deletion events and sends each deleted podName on
-// either vmDeletions or podDeletions as they occur, depending on whether the pod has the LabelVM
-// label.
+// watchPodEvents continuously tracks a handful of Pod-related events that we care about. These
+// events are: non-VM pod deletion, VM deletion, and VMs that disable scaling.
 //
 // This method starts its own goroutine, and guarantees that we have started listening for FUTURE
 // events once it returns (unless it returns error).
 //
 // Events occurring before this method is called will not be sent.
-func (e *AutoscaleEnforcer) watchPodDeletions(
-	ctx context.Context, vmDeletions chan<- api.PodName, podDeletions chan<- api.PodName,
+//
+// The reason we care about when scaling is disabled is that if we don't, we can run into the
+// following race condition:
+//
+//  1. VM created with autoscaling enabled
+//  2. Scheduler restarts and reads the state of the cluster. It records the difference between the
+//     VM's current and maximum usage as "buffer"
+//  3. Before the autoscaler-agent runner for the VM connects to the scheduler, the VM's label to
+//     enable autoscaling is removed, and the autoscaler-agent's runner exits.
+//  4. final state: The scheduler retains buffer for a VM that can't scale.
+//
+// To avoid (4) occurring, we track events where autoscaling is disabled for a VM and remove its
+// "buffer" when that happens. There's still some other possibilities for race conditions (FIXME),
+// but those are a little harder to handlle - in particular:
+//
+//  1. Scheduler exits
+//  2. autoscaler-agent runner downscales
+//  3. Scheduler starts, reads cluster state
+//  4. VM gets autoscaling disabled
+//  5. Scheduler removes the VM's buffer
+//  6. Before noticing that event, the autoscaler-agent upscales the VM and informs the scheduler of
+//     its current allocation (which it can do, because it was approved by a previous scheduler).
+//  7. The scheduler denies what it sees as upscaling.
+//
+// This one requires a very unlikely sequence of events to occur, that should be appropriately
+// handled by cancelled contexts in *almost all* cases.
+func (e *AutoscaleEnforcer) watchPodEvents(
+	ctx context.Context,
+	vmDeletions chan<- api.PodName,
+	vmDisabledScaling chan<- api.PodName,
+	podDeletions chan<- api.PodName,
 ) error {
 	// We're using the client-go cache here (indirectly through util.Watch) so that we don't miss
 	// deletion events. Otherwise, we can run into race conditions where events are missed in the
@@ -49,6 +80,19 @@ func (e *AutoscaleEnforcer) watchPodDeletions(
 		util.InitWatchModeSync, // note: doesn't matter, because AddFunc = nil.
 		metav1.ListOptions{},
 		util.WatchHandlerFuncs[*corev1.Pod]{
+			UpdateFunc: func(oldPod, newPod *corev1.Pod) {
+				// note: it doesn't matter whether we use newPod or oldPod for the name here; the
+				// pod name and namespace aren't allowed to change.
+				name := api.PodName{Name: newPod.Name, Namespace: newPod.Namespace}
+				_, isVM := oldPod.Labels[LabelVM]
+				if isVM && api.HasAutoscalingEnabled(oldPod) && !api.HasAutoscalingEnabled(newPod) {
+					klog.Infof(
+						"[autoscale-enforcer] watch: Received update to disable autoscaling for pod %v",
+						name,
+					)
+					vmDisabledScaling <- name
+				}
+			},
 			DeleteFunc: func(pod *corev1.Pod, mayBeStale bool) {
 				name := api.PodName{Name: pod.Name, Namespace: pod.Namespace}
 				klog.Infof("[autoscale-enforcer] watch: Received delete event for pod %v", name)

neondatabase/autoscaling

pkg/agent/globalstate.go
@@ -53,6 +53,7 @@ func vmIsOurResponsibility(vm *vmapi.VirtualMachine, config *Config, nodeName st
 	return vm.Status.Node == nodeName &&
 		vm.Status.Phase == vmapi.VmRunning &&
 		vm.Status.PodIP != "" &&
+		api.HasAutoscalingEnabled(vm) &&
 		vm.Spec.SchedulerName == config.Scheduler.SchedulerName
 }
 

pkg/plugin/plugin.go
@@ -86,12 +86,13 @@ func makeAutoscaleEnforcerPlugin(ctx context.Context, obj runtime.Object, h fram
 		}
 	}
 
-	// Start watching deletion events...
+	// Start watching Pod/VM events...
 	vmDeletions := make(chan util.NamespacedName)
+	vmDisabledScaling := make(chan util.NamespacedName)
 	podDeletions := make(chan util.NamespacedName)
-	klog.Infof("[autoscale-enforcer] Starting pod deletion watcher")
-	if err := p.watchPodDeletions(ctx, vmDeletions, podDeletions); err != nil {
-		return nil, fmt.Errorf("Error starting VM deletion watcher: %w", err)
+	klog.Infof("[autoscale-enforcer] Starting pod watcher")
+	if err := p.watchPodEvents(ctx, vmDeletions, vmDisabledScaling, podDeletions); err != nil {
+		return nil, fmt.Errorf("Error starting pod watcher: %w", err)
 	}
 
 	// ... but before handling the deletion events, read the current cluster state:
@@ -110,6 +111,8 @@ func makeAutoscaleEnforcerPlugin(ctx context.Context, obj runtime.Object, h fram
 				return
 			case name := <-vmDeletions:
 				p.handleVMDeletion(name)
+			case name := <-vmDisabledScaling:
+				p.handleVMDisabledScaling(name)
 			case name := <-podDeletions:
 				p.handlePodDeletion(name)
 			}

pkg/plugin/state.go
@@ -621,6 +621,30 @@ func (e *AutoscaleEnforcer) handleVMDeletion(podName util.NamespacedName) {
 	klog.Infof(fmtString, migrating, pod.name, pod.node.name, vCPUVerdict, memVerdict)
 }
 
+func (e *AutoscaleEnforcer) handleVMDisabledScaling(podName util.NamespacedName) {
+	klog.Infof("[autoscale-enforcer] Handling disabling autoscaling for VM pod %v", podName)
+
+	e.state.lock.Lock()
+	defer e.state.lock.Unlock()
+
+	pod, ok := e.state.podMap[podName]
+	if !ok {
+		klog.Warningf("[autoscale-enforcer] disable autoscaling: Cannot find pod %v in podMap", podName)
+		return
+	}
+
+	// Reset buffer to zero:
+	vCPUVerdict := collectResourceTransition(&pod.node.vCPU, &pod.vCPU).
+		handleAutoscalingDisabled()
+	memVerdict := collectResourceTransition(&pod.node.memSlots, &pod.memSlots).
+		handleAutoscalingDisabled()
+
+	fmtString := "[autoscale-enforcer] Disabled autoscaling for VM pod %v in node %s:\n" +
+		"\tvCPU verdict: %s\n" +
+		"\t mem verdict: %s"
+	klog.Infof(fmtString, pod.name, pod.node.name, vCPUVerdict, memVerdict)
+}
+
 func (e *AutoscaleEnforcer) handlePodDeletion(podName util.NamespacedName) {
 	klog.Infof("[autoscale-enforcer] Handling deletion of non-VM pod %v", podName)
 
@@ -881,6 +905,16 @@ func (p *AutoscaleEnforcer) readClusterState(ctx context.Context) error {
 
 			testingOnlyAlwaysMigrate: vmInfo.AlwaysMigrate,
 		}
+
+		// If scaling isn't enabled, then we can be more precise about usage.
+		if !vmInfo.ScalingEnabled {
+			ps.vCPU.Buffer = 0
+			ps.vCPU.Reserved = vmInfo.Cpu.Use
+
+			ps.memSlots.Buffer = 0
+			ps.memSlots.Reserved = vmInfo.Mem.Use
+		}
+
 		oldNodeVCPUReserved := ns.vCPU.Reserved
 		oldNodeMemReserved := ns.memSlots.Reserved
 		oldNodeVCPUBuffer := ns.vCPU.Buffer

pkg/plugin/watch.go
@@ -1,5 +1,8 @@
 package plugin
 
+// Implementation of watching Pods for Pod/VM deletions and changes so that a VM's autoscaling is
+// disabled.
+
 // Implementation of watching for VM deletions and VM migration completions, so we can unreserve the
 // associated resources
 
@@ -11,20 +14,47 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	klog "k8s.io/klog/v2"
 
+	"github.com/neondatabase/autoscaling/pkg/api"
 	"github.com/neondatabase/autoscaling/pkg/util"
 )
 
-// watchPodDeletions continuously tracks pod deletion events and sends each deleted podName on
-// either vmDeletions or podDeletions as they occur, depending on whether the pod has the LabelVM
-// label.
+// watchPodEvents continuously tracks a handful of Pod-related events that we care about. These
+// events are: non-VM pod deletion, VM deletion, and VMs that disable scaling.
 //
 // This method starts its own goroutine, and guarantees that we have started listening for FUTURE
 // events once it returns (unless it returns error).
 //
 // Events occurring before this method is called will not be sent.
-func (e *AutoscaleEnforcer) watchPodDeletions(
+//
+// The reason we care about when scaling is disabled is that if we don't, we can run into the
+// following race condition:
+//
+//  1. VM created with autoscaling enabled
+//  2. Scheduler restarts and reads the state of the cluster. It records the difference between the
+//     VM's current and maximum usage as "buffer"
+//  3. Before the autoscaler-agent runner for the VM connects to the scheduler, the VM's label to
+//     enable autoscaling is removed, and the autoscaler-agent's runner exits.
+//  4. final state: The scheduler retains buffer for a VM that can't scale.
+//
+// To avoid (4) occurring, we track events where autoscaling is disabled for a VM and remove its
+// "buffer" when that happens. There's still some other possibilities for race conditions (FIXME),
+// but those are a little harder to handlle - in particular:
+//
+//  1. Scheduler exits
+//  2. autoscaler-agent runner downscales
+//  3. Scheduler starts, reads cluster state
+//  4. VM gets autoscaling disabled
+//  5. Scheduler removes the VM's buffer
+//  6. Before noticing that event, the autoscaler-agent upscales the VM and informs the scheduler of
+//     its current allocation (which it can do, because it was approved by a previous scheduler).
+//  7. The scheduler denies what it sees as upscaling.
+//
+// This one requires a very unlikely sequence of events to occur, that should be appropriately
+// handled by cancelled contexts in *almost all* cases.
+func (e *AutoscaleEnforcer) watchPodEvents(
 	ctx context.Context,
 	vmDeletions chan<- util.NamespacedName,
+	vmDisabledScaling chan<- util.NamespacedName,
 	podDeletions chan<- util.NamespacedName,
 ) error {
 	_, err := util.Watch(
@@ -44,6 +74,19 @@ func (e *AutoscaleEnforcer) watchPodDeletions(
 		util.InitWatchModeSync, // note: doesn't matter, because AddFunc = nil.
 		metav1.ListOptions{},
 		util.WatchHandlerFuncs[*corev1.Pod]{
+			UpdateFunc: func(oldPod, newPod *corev1.Pod) {
+				// note: it doesn't matter whether we use newPod or oldPod for the name here; the
+				// pod name and namespace aren't allowed to change.
+				name := util.NamespacedName{Name: newPod.Name, Namespace: newPod.Namespace}
+				_, isVM := oldPod.Labels[LabelVM]
+				if isVM && api.HasAutoscalingEnabled(oldPod) && !api.HasAutoscalingEnabled(newPod) {
+					klog.Infof(
+						"[autoscale-enforcer] watch: Received update to disable autoscaling for pod %v",
+						name,
+					)
+					vmDisabledScaling <- name
+				}
+			},
 			DeleteFunc: func(pod *corev1.Pod, mayBeStale bool) {
 				name := util.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}
 				klog.Infof("[autoscale-enforcer] watch: Received delete event for pod %v", name)