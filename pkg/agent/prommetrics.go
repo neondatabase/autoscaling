e777b985d236140b5578b6a12f492aa1906447e5
sharnoff <sharnoff@neon.tech>
2023-04-14 18:19:33 +0300

    Rename prometheus metrics, prefix with "autoscaling_"

--- pkg/agent/prommetrics.go
+++ pkg/agent/prommetrics.go
@@ -14,28 +14,28 @@ type PromMetrics struct {
 func makePrometheusParts(globalstate *agentState) (PromMetrics, *prometheus.Registry) {
 	schedulerRequests := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "scheduler_plugin_requests_total",
-			Help: "Number of attempted HTTP requests to the scheduler plugin",
+			Name: "autoscaling_agent_scheduler_plugin_requests_total",
+			Help: "Number of attempted HTTP requests to the scheduler plugin by autoscaler-agents",
 		},
 		[]string{"code"},
 	)
 	informantRequestsOutbound := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "informant_outbound_requests_total",
-			Help: "Number of attempted HTTP requests to vm-informants",
+			Name: "autoscaling_agent_informant_outbound_requests_total",
+			Help: "Number of attempted HTTP requests to vm-informants by autoscaler-agents",
 		},
 		[]string{"code"},
 	)
 	informantRequestsInbound := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "informant_inbound_requests_total",
-			Help: "Number of HTTP requests from vm-informants",
+			Name: "autoscaling_agent_informant_inbound_requests_total",
+			Help: "Number of HTTP requests from vm-informants received by autoscaler-agents",
 		},
 		[]string{"endpoint", "code"},
 	)
 	totalVMs := prometheus.NewGaugeFunc(
 		prometheus.GaugeOpts{
-			Name: "tracked_vms_current",
+			Name: "autoscaling_agent_tracked_vms_current",
 			Help: "Number of VMs on the autoscaler-agent's node that it's tracking",
 		},
 		func() float64 {
