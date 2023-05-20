71f593c09b356985f2d9a2f05bc99ea77fce35ee
sharnoff <sharnoff@neon.tech>
2023-05-19 22:41:28 -0700

    neonvm/builder: Make output prettier

--- neonvm/tools/vm-builder-generic/main.go
+++ neonvm/tools/vm-builder-generic/main.go
@@ -5,6 +5,7 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -195,10 +196,21 @@ var (
 	forcePull  = flag.Bool("pull", false, `Pull src image even if already present locally`)
 )
 
+type dockerMessage struct {
+	Stream string `json:"stream"`
+}
+
 func printReader(reader io.ReadCloser) error {
 	scanner := bufio.NewScanner(reader)
 	for scanner.Scan() {
-		log.Println(scanner.Text())
+		candidateJSON := scanner.Bytes()
+		var msg dockerMessage
+		if err := json.Unmarshal(candidateJSON, &msg); err != nil || msg.Stream == "" {
+			log.Println(string(candidateJSON))
+			continue
+		}
+
+		log.Print(msg.Stream)
 	}
 	if err := scanner.Err(); err != nil {
 		return err
