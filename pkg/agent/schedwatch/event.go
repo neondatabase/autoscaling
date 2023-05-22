d79bfb7edd7487d2dc4be799cf3286ffeb3c92e0
sharnoff <sharnoff@neon.tech>
2023-05-22 16:03:34 -0700

    agent/schedwatch: Custom WatchEvent and SchedulerInfo formatting (#290)

--- pkg/agent/schedwatch/event.go
+++ pkg/agent/schedwatch/event.go
@@ -1,6 +1,8 @@
 package schedwatch
 
 import (
+	"fmt"
+
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 
@@ -10,6 +12,31 @@ import (
 type WatchEvent struct {
 	info SchedulerInfo
 	kind eventKind
+}
+
+func (ev WatchEvent) Format(state fmt.State, verb rune) {
+	switch {
+	case verb == 'v' && state.Flag('#'):
+		state.Write([]byte(fmt.Sprintf(
+			"schedwatch.WatchEvent{kind:%q, info:%#v}",
+			string(ev.kind), ev.info,
+		)))
+	default:
+		if verb != 'v' {
+			state.Write([]byte("%!"))
+			state.Write([]byte(string(verb)))
+			state.Write([]byte("(schedwatch.WatchEvent="))
+		}
+
+		state.Write([]byte(fmt.Sprintf(
+			"{kind:%v info:%v}",
+			ev.kind, ev.info,
+		)))
+
+		if verb != 'v' {
+			state.Write([]byte{')'})
+		}
+	}
 }
 
 type eventKind string
@@ -30,5 +57,30 @@ func newSchedulerInfo(pod *corev1.Pod) SchedulerInfo {
 		PodName: util.NamespacedName{Name: pod.Name, Namespace: pod.Namespace},
 		UID:     pod.UID,
 		IP:      pod.Status.PodIP,
+	}
+}
+
+func (info SchedulerInfo) Format(state fmt.State, verb rune) {
+	switch {
+	case verb == 'v' && state.Flag('#'):
+		state.Write([]byte(fmt.Sprintf(
+			"schedwatch.SchedulerInfo{PodName:%#v, IP:%q, UID:%q}",
+			info.PodName, info.IP, string(info.UID),
+		)))
+	default:
+		if verb != 'v' {
+			state.Write([]byte("%!"))
+			state.Write([]byte(string(verb)))
+			state.Write([]byte("(schedwatch.SchedulerInfo="))
+		}
+
+		state.Write([]byte(fmt.Sprintf(
+			"{PodName:%v IP:%q UID:%q}",
+			info.PodName, info.IP, info.UID,
+		)))
+
+		if verb != 'v' {
+			state.Write([]byte{')'})
+		}
 	}
 }
