d5cb880b40a63248c6ef421dc13f83be2ac64e11
Em Sharnoff <sharnoff@neon.tech>
2023-06-26 11:00:11 -0700

    plugin: Fix filter cycle metrics (#356)
    The previous implementation doesn't *really* work because it turns out
    PostFilter is only called if all Filter calls failed - so we some other
    way to count the number of filter cycles.
    
    This PR removes two metrics:
    
    * autoscaling_plugin_filter_cycle_successes_total
    * autoscaling_plugin_filter_cycle_rejections_total
    
    The new recommended flow is:
    
    * Get attempts with autoscaling_plugin_extension_calls_total{method="PreFilter"}
    * Get rejections with autoscaling_plugin_extension_calls_total{method="PostFilter"}
    * Successes can be calculated by the difference of the two.

--- pkg/plugin/prommetrics.go
+++ pkg/plugin/prommetrics.go
@@ -14,8 +14,6 @@ import (
 type PromMetrics struct {
 	pluginCalls           *prometheus.CounterVec
 	pluginCallFails       *prometheus.CounterVec
-	filterCycleSuccesses  prometheus.Counter
-	filterCycleRejections *prometheus.CounterVec
 	resourceRequests      *prometheus.CounterVec
 	validResourceRequests *prometheus.CounterVec
 }
@@ -46,19 +44,6 @@ func (p *AutoscaleEnforcer) makePrometheusRegistry() *prometheus.Registry {
 				Help: "Number of unsuccessful calls to scheduler plugin extension points",
 			},
 			[]string{"method", "status"},
-		)),
-		filterCycleSuccesses: util.RegisterMetric(reg, prometheus.NewCounter(
-			prometheus.CounterOpts{
-				Name: "autoscaling_plugin_filter_cycle_successes_total",
-				Help: "Number of successful Filter stages for any pod",
-			},
-		)),
-		filterCycleRejections: util.RegisterMetric(reg, prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Name: "autoscaling_plugin_filter_cycle_rejections_total",
-				Help: "For each pod, number of times rejected by *all* Filter evaluations",
-			},
-			[]string{"pod_name"},
 		)),
 		resourceRequests: util.RegisterMetric(reg, prometheus.NewCounterVec(
 			prometheus.CounterOpts{
