215c13ade3be0fa781cbf0214e9708420bd463e7
Em Sharnoff <sharnoff@neon.tech>
2023-06-30 18:16:19 -0700

    agent/billing: Move push logic into separate thread
    This should resolve some of the ongoing issues we've had with billing
    push requests timing out, because *currently* the push timeout must be
    short in order to produce correct data.
    
    Also, this commit removes pkg/billing.Batch, because this commit changes
    the agent to use its own batching, meaning pkg/billing.Batch is no
    longer required.
    
    Billing config changes:
    
    - Renamed pushTimeoutSeconds to pushRequestTimeoutSeconds
    - Added accumulateEverySeconds (now distinct from pushing!)
    - Added maxBatchSize
    
    Billing metrics changes:
    
    - Renamed autoscaling_agent_billing_batch_size to autoscaling_agent_billing_queue_size
    - Added autoscaling_agent_billing_last_send_duration_seconds

---
+++ pkg/agent/billing/queue.go
@@ -0,0 +1,79 @@
+package billing
+
+// Implementation of the event queue for mediating event generation and event sending.
+//
+// The "public" (ish - it's all one package) types are eventQueuePuller and eventQueuePusher, two
+// halves of the same queue. Each half is only safe for use from a single thread, but *together*
+// they can be used in separate threads.
+
+import (
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"golang.org/x/exp/slices"
+
+	"github.com/neondatabase/autoscaling/pkg/util"
+)
+
+// this is generic just so there's less typing - "billing.IncrementalEvent" is long!
+type eventQueueInternals[E any] struct {
+	mu        sync.Mutex
+	items     []E
+	sizeGauge prometheus.Gauge
+}
+
+type eventQueuePuller[E any] struct {
+	internals *eventQueueInternals[E]
+}
+
+type eventQueuePusher[E any] struct {
+	internals *eventQueueInternals[E]
+}
+
+func newEventQueue[E any](sizeGauge prometheus.Gauge) (eventQueuePusher[E], eventQueuePuller[E]) {
+	internals := &eventQueueInternals[E]{
+		mu:        sync.Mutex{},
+		items:     nil,
+		sizeGauge: sizeGauge,
+	}
+	return eventQueuePusher[E]{internals}, eventQueuePuller[E]{internals}
+}
+
+// NB: must hold mu
+func (qi *eventQueueInternals[E]) updateGauge() {
+	qi.sizeGauge.Set(float64(len(qi.items)))
+}
+
+func (q eventQueuePusher[E]) enqueue(events ...E) {
+	q.internals.mu.Lock()
+	defer q.internals.mu.Unlock()
+
+	q.internals.items = append(q.internals.items, events...)
+	q.internals.updateGauge()
+}
+
+func (q eventQueuePuller[E]) size() int {
+	q.internals.mu.Lock()
+	defer q.internals.mu.Unlock()
+
+	return len(q.internals.items)
+}
+
+func (q eventQueuePuller[E]) get(limit int) []E {
+	q.internals.mu.Lock()
+	defer q.internals.mu.Unlock()
+
+	count := util.Min(limit, len(q.internals.items))
+	// NOTE: this kind of access escaping the mutex is only sound because this access is only
+	// granted to the puller, and there's only one puller, and it isn't sound to use the output of a
+	// previous get() after calling drop().
+	return q.internals.items[:count]
+}
+
+func (q eventQueuePuller[E]) drop(count int) {
+	q.internals.mu.Lock()
+	defer q.internals.mu.Unlock()
+
+	q.internals.items = slices.Replace(q.internals.items, 0, count)
+	q.internals.updateGauge()
+}
---
+++ pkg/agent/billing/send.go
@@ -0,0 +1,143 @@
+package billing
+
+// Logic responsible for sending billing events by repeatedly pulling from the eventQueue
+
+import (
+	"context"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/neondatabase/autoscaling/pkg/billing"
+	"github.com/neondatabase/autoscaling/pkg/util"
+)
+
+type eventSender struct {
+	client            billing.Client
+	config            *Config
+	metrics           PromMetrics
+	queue             eventQueuePuller[*billing.IncrementalEvent]
+	collectorFinished util.CondChannelReceiver
+
+	// lastSendDuration tracks the "real" last full duration of (eventSender).sendAllCurrentEvents().
+	//
+	// It's separate from metrics.lastSendDuration because (a) we'd like to include the duration of
+	// ongoing calls to sendAllCurrentEvents, but (b) we don't want the bias towards lower durations
+	// that comes with that.
+	//
+	// Here's some more detail:
+	//
+	// To make sure that long-running sendAllCurrentEvents() loops show up in the metrics while
+	// they're still running, we want to periodically update metrics.lastSendDuration before the
+	// loop has finished. A side-effect of doing this naively is that the gauge will sometimes
+	// return durations that are much shorter than the *actual* previous send loop duration.
+	//
+	// In order to fix this, we store that *actual* previous duration in this field, but and only
+	// update the metric when either (a) the loop is done, or (b) the duration so far is already
+	// longer than the previous one.
+	//
+	// This means that we remove the bias towards shorter durations, at the expense of sometimes
+	// returning higher durations for too long. IMO that's ok, and we'd rather have our metrics give
+	// a pessimistic but more accurate view.
+	lastSendDuration time.Duration
+}
+
+func (s eventSender) senderLoop(logger *zap.Logger) {
+	ticker := time.NewTicker(time.Second * time.Duration(s.config.PushEverySeconds))
+	defer ticker.Stop()
+
+	for {
+		final := false
+
+		select {
+		case <-s.collectorFinished.Recv():
+			logger.Info("Received notification that collector finished")
+			final = true
+		case <-ticker.C:
+			logger.Info("Starting periodic push")
+		}
+
+		s.sendAllCurrentEvents(logger)
+
+		if final {
+			logger.Info("Ending events sender loop")
+			return
+		}
+	}
+}
+
+func (s eventSender) sendAllCurrentEvents(logger *zap.Logger) {
+	if s.queue.size() == 0 {
+		logger.Info("No billing events to push")
+		s.lastSendDuration = 0
+		s.metrics.lastSendDuration.Set(1e-6) // small value, to indicate that nothing happened
+		return
+	}
+
+	logger.Info("Pushing all available events")
+	total := 0
+	startTime := time.Now()
+
+	// while there's still events in the queue, send them
+	//
+	// If events are being added to the queue faster than we cna
+	for {
+		chunk := s.queue.get(int(s.config.MaxBatchSize))
+		count := len(chunk)
+		if count == 0 {
+			totalTime := time.Since(startTime)
+			s.lastSendDuration = totalTime
+			s.metrics.lastSendDuration.Set(totalTime.Seconds())
+
+			logger.Info(
+				"All available events have been sent",
+				zap.Int("total", total),
+				zap.Duration("totalTime", totalTime),
+			)
+			return
+		}
+
+		reqStart := time.Now()
+		err := func() error {
+			reqCtx, cancel := context.WithTimeout(context.TODO(), time.Second*time.Duration(s.config.PushRequestTimeoutSeconds))
+			defer cancel()
+
+			return billing.Send(reqCtx, s.client, chunk)
+		}()
+		reqDuration := time.Since(reqStart)
+
+		if err != nil {
+			// Something went wrong and we're going to abandon attempting to push any further
+			// events.
+			logger.Error(
+				"Failed to push billing events",
+				zap.Int("count", count),
+				zap.Duration("after", reqDuration),
+				zap.Int("total", total),
+				zap.Duration("totalTime", time.Since(startTime)),
+				zap.Error(err),
+			)
+			s.metrics.sendErrorsTotal.Inc()
+			s.lastSendDuration = 0
+			s.metrics.lastSendDuration.Set(0.0) // use 0 as a flag that something went wrong; there's no valid time here.
+			return
+		}
+
+		s.queue.drop(count) // mark len(chunk) as successfully processed
+		total += len(chunk)
+		currentTotalTime := time.Since(startTime)
+
+		logger.Info(
+			"Successfully pushed some billing events",
+			zap.Int("count", count),
+			zap.Duration("after", reqDuration),
+			zap.Int("total", total),
+			zap.Duration("totalTime", currentTotalTime),
+		)
+
+		if currentTotalTime > s.lastSendDuration {
+			s.lastSendDuration = currentTotalTime
+			s.metrics.lastSendDuration.Set(currentTotalTime.Seconds())
+		}
+	}
+}
