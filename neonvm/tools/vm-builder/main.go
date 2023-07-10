d1f85eadd956d152dc77dcd7095b4375814d3481
Felix Prasanna <felix@neon.tech>
2023-07-10 10:45:07 -0500

    Explicitly sequence monitor start before informant

--- neonvm/tools/vm-builder/main.go
+++ neonvm/tools/vm-builder/main.go
@@ -248,8 +248,7 @@ fi
 ::respawn:/neonvm/bin/acpid -f -c /neonvm/acpi
 ::respawn:/neonvm/bin/vector -c /neonvm/config/vector.yaml --config-dir /etc/vector
 ::respawn:/neonvm/bin/vmstart
-::respawn:su -p vm-informant -c '/usr/local/bin/vm-monitor --cgroup=neon-postgres{{if .FileCache}} --pgconnstr="dbname=postgres user=cloud_admin sslmode=disable"{{end}}'
-::respawn:su -p vm-informant -c '/usr/local/bin/vm-informant'
+::respawn:su -p vm-informant -c '/usr/local/bin/vm-monitor --cgroup=neon-postgres{{if .FileCache}} --pgconnstr="dbname=postgres user=cloud_admin sslmode=disable"{{end}} && /usr/local/bin/vm-informant'
 ::respawn:su -p nobody -c '/usr/local/bin/pgbouncer /etc/pgbouncer.ini'
 ::respawn:su -p nobody -c 'DATA_SOURCE_NAME="user=cloud_admin sslmode=disable dbname=postgres" /bin/postgres_exporter --auto-discover-databases --exclude-databases=template0,template1'
 ttyS0::respawn:/neonvm/bin/agetty --8bits --local-line --noissue --noclear --noreset --host console --login-program /neonvm/bin/login --login-pause --autologin root 115200 ttyS0 linux
