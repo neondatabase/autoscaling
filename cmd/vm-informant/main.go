e85a45a4311bdb23c27bbf14135d6f1f8808930a
Felix Prasanna <felix@neon.tech>
2023-07-10 11:55:55 -0500

    just use NewProduction for log config

--- cmd/vm-informant/main.go
+++ cmd/vm-informant/main.go
@@ -20,7 +20,7 @@ import (
 const minSubProcessRestartInterval = 5 * time.Second
 
 func main() {
-	logger := zap.Must(zap.NewProductionConfig().Build()).Named("vm-informant")
+	logger := zap.Must(zap.NewProduction()).Named("vm-informant")
 	defer logger.Sync() //nolint:errcheck // what are we gonna do, log something about it?
 
 	logger.Info("", zap.Any("buildInfo", util.GetBuildInfo()))
