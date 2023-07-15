neondatabase/autoscaling

commit 7d3207015f874eee64aeb51491feb2904cb17c69

plugin: Use event queue for pod/VM events (#160)
* plugin: Use event queue for pod/VM events

* use iterator

Co-authored-by: Sam Kleinman <sam@neon.tech>

---------

Co-authored-by: Sam Kleinman <sam@neon.tech>

pkg/plugin/plugin.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/tychoish/fun/pubsub"
+
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	scheme "k8s.io/client-go/kubernetes/scheme"
@@ -97,47 +99,53 @@ func makeAutoscaleEnforcerPlugin(ctx context.Context, obj runtime.Object, h fram
 		}
 	}
 
-	// Start watching Pod/VM events...
-	vmDeletions := make(chan util.NamespacedName)
-	vmDisabledScaling := make(chan util.NamespacedName)
-	podDeletions := make(chan util.NamespacedName)
-	vmBoundsChanged := make(chan vmPodInfo)
+	// Start watching Pod/VM events, adding them to a shared queue to process them in order
+	queue := pubsub.NewUnlimitedQueue[func()]()
+	pushToQueue := func(f func()) {
+		if err := queue.Add(f); err != nil {
+			klog.Warningf("[autoscale-enforcer] error adding to pod/VM event queue: %s", err)
+		}
+	}
+	submitVMPodDeletion := func(pod util.NamespacedName) {
+		pushToQueue(func() { p.handleVMDeletion(pod) })
+	}
+	submitNonVMPodDeletion := func(name util.NamespacedName) {
+		pushToQueue(func() { p.handlePodDeletion(name) })
+	}
+	submitVMDisabledScaling := func(pod util.NamespacedName) {
+		pushToQueue(func() { p.handleVMDisabledScaling(pod) })
+	}
+	submitVMBoundsChanged := func(vm *api.VmInfo, podName string) {
+		pushToQueue(func() { p.handleUpdatedScalingBounds(vm, podName) })
+	}
+
 	klog.Infof("[autoscale-enforcer] Starting pod watcher")
-	if err := p.watchPodEvents(ctx, vmDeletions, podDeletions); err != nil {
+	if err := p.watchPodEvents(ctx, submitVMPodDeletion, submitNonVMPodDeletion); err != nil {
 		return nil, fmt.Errorf("Error starting pod watcher: %w", err)
 	}
 
 	klog.Infof("[autoscale-enforcer] Starting VM watcher")
-	vmStore, err := p.watchVMEvents(ctx, vmDisabledScaling, vmBoundsChanged)
+	vmStore, err := p.watchVMEvents(ctx, submitVMDisabledScaling, submitVMBoundsChanged)
 	if err != nil {
 		return nil, fmt.Errorf("Error starting VM watcher: %w", err)
 	}
 
 	p.vmStore = util.NewIndexedWatchStore(vmStore, util.NewNameIndex[vmapi.VirtualMachine]())
 
-	// ... but before handling the deletion events, read the current cluster state:
+	// ... but before handling the events, read the current cluster state:
 	klog.Infof("[autoscale-enforcer] Reading initial cluster state")
 	if err = p.readClusterState(ctx); err != nil {
 		return nil, fmt.Errorf("Error reading cluster state: %w", err)
 	}
 
-	// TODO: this is a little clumsy; both channels are sent on by the same goroutine and both
-	// consumed by this one. This should be changed, probably with handleVMDeletion and
-	// handlePodDeletion being merged into one function.
 	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case name := <-vmDeletions:
-				p.handleVMDeletion(name)
-			case name := <-vmDisabledScaling:
-				p.handleVMDisabledScaling(name)
-			case name := <-podDeletions:
-				p.handlePodDeletion(name)
-			case v := <-vmBoundsChanged:
-				p.handleUpdatedScalingBounds(v.vm, v.podName)
-			}
+		iter := queue.Iterator()
+		for iter.Next(ctx) {
+			callback := iter.Value()
+			callback()
+		}
+		if err := iter.Close(); err != nil {
+			klog.Infof("[autoscale-enforcer] stopped waiting on pod/VM queue: %s", err)
 		}
 	}()
 

pkg/plugin/watch.go
@@ -27,8 +27,8 @@ import (
 // Events occurring before this method is called will not be sent.
 func (e *AutoscaleEnforcer) watchPodEvents(
 	ctx context.Context,
-	vmDeletions chan<- util.NamespacedName,
-	podDeletions chan<- util.NamespacedName,
+	submitVMDeletion func(util.NamespacedName),
+	submitPodDeletion func(util.NamespacedName),
 ) error {
 	_, err := util.Watch(
 		ctx,
@@ -51,9 +51,9 @@ func (e *AutoscaleEnforcer) watchPodEvents(
 				name := util.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}
 				klog.Infof("[autoscale-enforcer] watch: Received delete event for pod %v", name)
 				if _, ok := pod.Labels[LabelVM]; ok {
-					vmDeletions <- name
+					submitVMDeletion(name)
 				} else {
-					podDeletions <- name
+					submitPodDeletion(name)
 				}
 			},
 		},
@@ -63,11 +63,6 @@ func (e *AutoscaleEnforcer) watchPodEvents(
 	}
 
 	return nil
-}
-
-type vmPodInfo struct {
-	vm      *api.VmInfo
-	podName string
 }
 
 // watchVMEvents watches for changes in VMs: signaling when scaling becomes disabled and updating
@@ -100,8 +95,8 @@ type vmPodInfo struct {
 // handled by cancelled contexts in *almost all* cases.
 func (e *AutoscaleEnforcer) watchVMEvents(
 	ctx context.Context,
-	vmDisabledScaling chan<- util.NamespacedName,
-	updatedScalingBounds chan<- vmPodInfo,
+	submitVMDisabledScaling func(util.NamespacedName),
+	submitVMBoundsChanged func(_ *api.VmInfo, podName string),
 ) (*util.WatchStore[vmapi.VirtualMachine], error) {
 	return util.Watch(
 		ctx,
@@ -144,7 +139,7 @@ func (e *AutoscaleEnforcer) watchVMEvents(
 						"[autoscale-enforcer] watch: Received update to disable autoscaling for pod %v",
 						name,
 					)
-					vmDisabledScaling <- name
+					submitVMDisabledScaling(name)
 				}
 
 				// If the pod changed, then we're going to handle a deletion event for the old pod,
@@ -160,7 +155,7 @@ func (e *AutoscaleEnforcer) watchVMEvents(
 					return
 				}
 
-				updatedScalingBounds <- vmPodInfo{vm: newInfo, podName: newVM.Status.PodName}
+				submitVMBoundsChanged(newInfo, newVM.Status.PodName)
 			},
 		},
 	)

commit e34080a2b4a4671c4e20f0fd591919913132684a

plugin: Fix leak by actually consuming queue (#415)
Fixes #409, validated locally, etc. Turns out pubsub.Queue[...].Iterator()
doesn't actually consume the queue, it just visits everything in it.

Bug was originally introduced by #160, commit 2795b50, but we didn't
notice before because of (a) other memory leakage (see #248) and (b) the
size of the items in the queue were all small before #399, which started
passing through whole K8s Pod objects.

pkg/plugin/plugin.go
@@ -207,13 +207,15 @@ func makeAutoscaleEnforcerPlugin(
 	}
 
 	go func() {
-		iter := queue.Iterator()
-		for iter.Next(ctx) {
-			callback := iter.Value()
+		for {
+			callback, err := queue.Wait(ctx)
+			if err != nil {
+				logger.Info("Stopped waiting on pod/VM queue", zap.Error(err))
+				break
+			}
+
 			callback()
-		}
-		if err := iter.Close(); err != nil {
-			logger.Info("Stopped waiting on pod/VM queue", zap.Error(err))
+			queue.Remove()
 		}
 	}()
 