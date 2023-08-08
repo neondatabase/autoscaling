e46b2f15fe03bcd8aab028bc71d57e8c571e3c42
Heikki Linnakangas <heikki@neon.tech>
2023-08-08 22:01:33 +0300

    fix broken sentence in comment

--- pkg/agent/config.go
+++ pkg/agent/config.go
@@ -42,7 +42,7 @@ type InformantConfig struct {
 	// ServerPort is the port that the VM informant serves from
 	ServerPort uint16 `json:"serverPort"`
 
-	// CallbackPort is that the agent listens on for informant -> agent requests
+	// CallbackPort is the port that the agent listens on for informant -> agent requests
 	CallbackPort int `json:"callbackPort"`
 
 	// RetryServerMinWaitSeconds gives the minimum duration, in seconds, that we must wait between the
