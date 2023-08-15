6c08b36b7f411c25d8fac4b352862207cf8fd05e
Em Sharnoff <sharnoff@neon.tech>
2023-08-15 15:57:55 -0700

    Disable zap's enabled-by-default log sampling (#475)

--- cmd/autoscaler-agent/main.go
+++ cmd/autoscaler-agent/main.go
@@ -21,7 +21,9 @@ import (
 )
 
 func main() {
-	logger := zap.Must(zap.NewProduction()).Named("autoscaler-agent")
+	logConfig := zap.NewProductionConfig()
+	logConfig.Sampling = nil // Disable sampling, which the production config enables by default.
+	logger := zap.Must(logConfig.Build()).Named("autoscaler-agent")
 	defer logger.Sync() //nolint:errcheck // what are we gonna do, log something about it?
 
 	logger.Info("", zap.Any("buildInfo", util.GetBuildInfo()))
