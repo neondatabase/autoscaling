bc6893941eddf863107b4418946f8ef53fe4e631
Em Sharnoff <sharnoff@neon.tech>
2023-10-11 13:13:04 -0700

    explicitly ignore errors

--- pkg/agent/core/action.go
+++ pkg/agent/core/action.go
@@ -51,11 +51,11 @@ func addObjectPtr[T zapcore.ObjectMarshaler](enc zapcore.ObjectEncoder, key stri
 }
 
 func (s ActionSet) MarshalLogObject(enc zapcore.ObjectEncoder) error {
-	addObjectPtr(enc, "wait", s.Wait)
-	addObjectPtr(enc, "pluginRequest", s.PluginRequest)
-	addObjectPtr(enc, "neonvmRequest", s.NeonVMRequest)
-	addObjectPtr(enc, "monitorDownscale", s.MonitorDownscale)
-	addObjectPtr(enc, "monitorUpscale", s.MonitorUpscale)
+	_ = addObjectPtr(enc, "wait", s.Wait)
+	_ = addObjectPtr(enc, "pluginRequest", s.PluginRequest)
+	_ = addObjectPtr(enc, "neonvmRequest", s.NeonVMRequest)
+	_ = addObjectPtr(enc, "monitorDownscale", s.MonitorDownscale)
+	_ = addObjectPtr(enc, "monitorUpscale", s.MonitorUpscale)
 	return nil
 }
 
@@ -67,29 +67,29 @@ func (a ActionWait) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 
 // MarshalLogObject implements zapcore.ObjectMarshaler, so that ActionPluginRequest can be used with zap.Object
 func (a ActionPluginRequest) MarshalLogObject(enc zapcore.ObjectEncoder) error {
-	addObjectPtr(enc, "lastPermit", a.LastPermit)
-	enc.AddObject("target", a.Target)
-	enc.AddReflected("metrics", a.Metrics)
+	_ = addObjectPtr(enc, "lastPermit", a.LastPermit)
+	_ = enc.AddObject("target", a.Target)
+	_ = enc.AddReflected("metrics", a.Metrics)
 	return nil
 }
 
 // MarshalLogObject implements zapcore.ObjectMarshaler, so that ActionNeonVMRequest can be used with zap.Object
 func (a ActionNeonVMRequest) MarshalLogObject(enc zapcore.ObjectEncoder) error {
-	enc.AddObject("current", a.Current)
-	enc.AddObject("target", a.Target)
+	_ = enc.AddObject("current", a.Current)
+	_ = enc.AddObject("target", a.Target)
 	return nil
 }
 
 // MarshalLogObject implements zapcore.ObjectMarshaler, so that ActionMonitorDownscale can be used with zap.Object
 func (a ActionMonitorDownscale) MarshalLogObject(enc zapcore.ObjectEncoder) error {
-	enc.AddObject("current", a.Current)
-	enc.AddObject("target", a.Target)
+	_ = enc.AddObject("current", a.Current)
+	_ = enc.AddObject("target", a.Target)
 	return nil
 }
 
 // MarshalLogObject implements zapcore.ObjectMarshaler, so that ActionMonitorUpscale can be used with zap.Object
 func (a ActionMonitorUpscale) MarshalLogObject(enc zapcore.ObjectEncoder) error {
-	enc.AddObject("current", a.Current)
-	enc.AddObject("target", a.Target)
+	_ = enc.AddObject("current", a.Current)
+	_ = enc.AddObject("target", a.Target)
 	return nil
 }
