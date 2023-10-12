166c8d01fa12c1d8eac2a586403bd83648349d73
Em Sharnoff <sharnoff@neon.tech>
2023-10-12 15:32:57 -0700

    util: Move patch.go into its own package (#535)
    Also, rename the items now that they're redundant with the package name.

--- pkg/agent/runner.go
+++ pkg/agent/runner.go
@@ -36,6 +36,7 @@ import (
 	"github.com/neondatabase/autoscaling/pkg/agent/schedwatch"
 	"github.com/neondatabase/autoscaling/pkg/api"
 	"github.com/neondatabase/autoscaling/pkg/util"
+	"github.com/neondatabase/autoscaling/pkg/util/patch"
 )
 
 // PluginProtocolVersion is the current version of the agent<->scheduler plugin in use by this
@@ -714,12 +715,12 @@ func (r *Runner) doMetricsRequest(
 }
 
 func (r *Runner) doNeonVMRequest(ctx context.Context, target api.Resources) error {
-	patches := []util.JSONPatch{{
-		Op:    util.PatchReplace,
+	patches := []patch.Operation{{
+		Op:    patch.OpReplace,
 		Path:  "/spec/guest/cpus/use",
 		Value: target.VCPU.ToResourceQuantity(),
 	}, {
-		Op:    util.PatchReplace,
+		Op:    patch.OpReplace,
 		Path:  "/spec/guest/memorySlots/use",
 		Value: target.Mem,
 	}}
