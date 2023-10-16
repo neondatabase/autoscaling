b3503f1aa025157acf59d29ca0a84f65bb9cfdfd
Alexander Bayandin <alexander@neon.tech>
2023-10-16 13:02:37 +0100

    Update pgbouncer to 1.21.0

--- neonvm/tools/vm-builder/main.go
+++ neonvm/tools/vm-builder/main.go
@@ -74,8 +74,8 @@ RUN set -e \
 		libevent-dev \
 		libssl-dev
 
-ENV PGBOUNCER_VERSION 1.18.0
-ENV PGBOUNCER_GITPATH 1_18_0
+ENV PGBOUNCER_VERSION 1.21.0
+ENV PGBOUNCER_GITPATH 1_21_0
 RUN set -e \
 	&& curl -sfSL https://github.com/pgbouncer/pgbouncer/releases/download/pgbouncer_${PGBOUNCER_GITPATH}/pgbouncer-${PGBOUNCER_VERSION}.tar.gz -o pgbouncer-${PGBOUNCER_VERSION}.tar.gz \
 	&& tar xzvf pgbouncer-${PGBOUNCER_VERSION}.tar.gz \
