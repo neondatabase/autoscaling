6ed595aa43557637671b47b3b99bb541e8142e8a
Em Sharnoff <sharnoff@neon.tech>
2024-04-05 17:39:51 -0700

    agent: Make metrics parsing generic (#895)
    Also, switch from using custom parsing to the proper parser provided by
    the prometheus package(s).
    
    Also also, change the global config from specifying LoadPrefix to
    instead providing the names of all of the system metrics we need.

--- go.mod
+++ go.mod
@@ -61,6 +61,8 @@ require (
 	github.com/onsi/gomega v1.24.2
 	github.com/opencontainers/runtime-spec v1.0.3-0.20210326190908-1c3f411f0417
 	github.com/prometheus/client_golang v1.14.0
+	github.com/prometheus/client_model v0.3.0
+	github.com/prometheus/common v0.37.0
 	github.com/samber/lo v1.39.0
 	github.com/stretchr/testify v1.8.1
 	github.com/tychoish/fun v0.8.5
@@ -166,8 +168,6 @@ require (
 	github.com/opencontainers/selinux v1.10.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/prometheus/client_model v0.3.0 // indirect
-	github.com/prometheus/common v0.37.0 // indirect
 	github.com/prometheus/procfs v0.8.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spf13/cobra v1.6.1 // indirect
--- pkg/agent/core/metrics.go
+++ pkg/agent/core/metrics.go
@@ -4,78 +4,93 @@ package core
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
+	"io"
+
+	promtypes "github.com/prometheus/client_model/go"
+	promfmt "github.com/prometheus/common/expfmt"
+	"github.com/tychoish/fun/erc"
 
 	"github.com/neondatabase/autoscaling/pkg/api"
 )
 
 type Metrics struct {
-	LoadAverage1Min  float32
-	MemoryUsageBytes float32
+	LoadAverage1Min  float64
+	MemoryUsageBytes float64
 }
 
 func (m Metrics) ToAPI() api.Metrics {
 	return api.Metrics{
-		LoadAverage1Min:  m.LoadAverage1Min,
+		LoadAverage1Min:  float32(m.LoadAverage1Min),
 		LoadAverage5Min:  nil,
 		MemoryUsageBytes: nil,
 	}
 }
 
-// ReadMetrics generates Metrics from vector.dev's host metrics output, or returns error on failure
-//
-// This function could be more efficient, but realistically it doesn't matter. The size of the
-// output from node_exporter/vector is so small anyways.
-func ReadMetrics(nodeExporterOutput []byte, loadPrefix string) (m Metrics, err error) {
-	lines := strings.Split(string(nodeExporterOutput), "\n")
+// FromPrometheus represents metric types that can be parsed from prometheus output.
+type FromPrometheus interface {
+	fromPrometheus(map[string]*promtypes.MetricFamily) error
+}
 
-	getField := func(linePrefix, dontMatch string) (float32, error) {
-		var line string
-		for _, l := range lines {
-			if strings.HasPrefix(l, linePrefix) && (len(dontMatch) == 0 || !strings.HasPrefix(l, dontMatch)) {
-				line = l
-				break
-			}
-		}
-		if line == "" {
-			return 0, fmt.Errorf("No line in metrics output starting with %q", linePrefix)
-		}
-
-		fields := strings.Fields(line)
-		if len(fields) < 2 {
-			return 0, fmt.Errorf(
-				"Expected >= 2 fields in metrics output for %q. Got %v",
-				linePrefix, len(fields),
-			)
-		}
-
-		v, err := strconv.ParseFloat(fields[1], 32)
-		if err != nil {
-			return 0, fmt.Errorf(
-				"Error parsing %q as float for line starting with %q: %w",
-				fields[1], linePrefix, err,
-			)
-		}
-		return float32(v), nil
-	}
-
-	m.LoadAverage1Min, err = getField(loadPrefix+"load1", loadPrefix+"load15")
+// ParseMetrics reads the prometheus text-format content, parses it, and uses M's implementation of
+// FromPrometheus to populate it before returning.
+func ParseMetrics(content io.Reader, metrics FromPrometheus) error {
+	var parser promfmt.TextParser
+	mfs, err := parser.TextToMetricFamilies(content)
 	if err != nil {
-		return
+		return fmt.Errorf("failed to parse content as prometheus text format: %w", err)
 	}
 
-	availableMem, err := getField(loadPrefix+"memory_available_bytes", "")
-	if err != nil {
-		return
-	}
-	totalMem, err := getField(loadPrefix+"memory_total_bytes", "")
-	if err != nil {
-		return
+	if err := metrics.fromPrometheus(mfs); err != nil {
+		return fmt.Errorf("failed to extract metrics: %w", err)
 	}
 
-	// Add an extra 100 MiB to account for kernel memory usage
-	m.MemoryUsageBytes = totalMem - availableMem + 100*(1<<20)
+	return nil
+}
 
-	return
+func extractFloatGauge(mf *promtypes.MetricFamily) (float64, error) {
+	if mf.GetType() != promtypes.MetricType_GAUGE {
+		return 0, fmt.Errorf("wrong metric type: expected %s but got %s", promtypes.MetricType_GAUGE, mf.GetType())
+	} else if len(mf.Metric) != 1 {
+		return 0, fmt.Errorf("expected 1 metric, found %d", len(mf.Metric))
+	}
+
+	return mf.Metric[0].GetGauge().GetValue(), nil
+}
+
+// Helper function to return an error for a missing metric
+func missingMetric(name string) error {
+	return fmt.Errorf("missing expected metric %s", name)
+}
+
+// fromPrometheus implements FromPrometheus, so Metrics can be used with ParseMetrics.
+func (m *Metrics) fromPrometheus(mfs map[string]*promtypes.MetricFamily) error {
+	ec := &erc.Collector{}
+
+	getFloat := func(metricName string) float64 {
+		if mf := mfs[metricName]; mf != nil {
+			f, err := extractFloatGauge(mf)
+			ec.Add(err) // does nothing if err == nil
+			return f
+		} else {
+			ec.Add(missingMetric(metricName))
+			return 0
+		}
+	}
+
+	load1 := getFloat("host_load1")
+	memTotal := getFloat("host_memory_total_bytes")
+	memAvailable := getFloat("host_memory_available_bytes")
+
+	tmp := Metrics{
+		LoadAverage1Min: load1,
+		// Add an extra 100 MiB to account for kernel memory usage
+		MemoryUsageBytes: memTotal - memAvailable + 100*(1<<20),
+	}
+
+	if err := ec.Resolve(); err != nil {
+		return err
+	}
+
+	*m = tmp
+	return nil
 }
