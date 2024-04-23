4d64ea77ac83f66014cbae5a017fa52b579a9ef9
Oleg Vasilev <oleg@neon.tech>
2024-04-23 17:25:14 +0300

    rewrite send
    Signed-off-by: Oleg Vasilev <oleg@neon.tech>

--- pkg/billing/client.go
+++ pkg/billing/client.go
@@ -142,13 +142,19 @@ func (c S3Client) LogFields() zap.Field {
 }
 
 func (c S3Client) send(ctx context.Context, payload []byte, _ TraceID) error {
+	// Source of truth for the storage format:
+	// https://github.com/neondatabase/cloud/issues/11199#issuecomment-1992549672
+
 	key := c.generateKey()
 	buf := bytes.Buffer{}
 
 	gzW := gzip.NewWriter(&buf)
 	_, err := gzW.Write(payload)
-	_ = gzW.Close()
+	if err != nil {
+		return RequestError{Err: err}
+	}
 
+	err = gzW.Close() // Have to close it before reading the buffer
 	if err != nil {
 		return RequestError{Err: err}
 	}
