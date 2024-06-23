457f4329259df2dbd84383eb4e503da1e1281d0a
Em Sharnoff <sharnoff@neon.tech>
2024-06-23 13:10:15 -0700

    Update pkg/plugin/plugin.go
    Co-authored-by: Oleg Vasilev <oleg@neon.tech>

--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -655,7 +655,7 @@ func (e *AutoscaleEnforcer) Score(
 	}
 
 	// Special case: return minimum score if we don't have room
-	overbudget, verdict := e.speculativeReserve(node, vmInfo, pod, false, func(_ verdictSet, overBudget bool) bool {
+	overbudget, verdict := e.speculativeReserve(node, vmInfo, pod, false, func(_ verdictSet, _ bool) bool {
 		return false // never actually accept the pod; we're just doing this to ask if it's over-budget.
 	})
 	if overbudget {
