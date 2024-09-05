dbcc2b7eab1da1330304de373d68e31f44f2a631
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2024-09-05 18:39:55 +0200

    Add debug info to quotas script

--- neonvm/tools/vm-builder/files/set-disk-quota.sh
+++ neonvm/tools/vm-builder/files/set-disk-quota.sh
@@ -8,10 +8,14 @@ if [ "$#" -eq 2 ]; then
     size="$1"
     mountpoint="$2"
 else
-    echo "expected 1 or 2 args, got $#" >&2
+    echo "expected 2 args, got $#" >&2
     echo "$USAGE" >&2
     exit 1
 fi
 
+echo "Setting disk quota for $mountpoint to $size KB"
 /neonvm/bin/setquota -P 0 0 "$size" 0 0 "$mountpoint"
+
+echo "DONE! Disk quota set"
+/neonvm/bin/repquota -P -a
 
