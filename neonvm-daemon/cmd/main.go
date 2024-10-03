50b71803a1b575e5b476fe06e513961f0db0ad82
Misha Sakhnov <misha@neon.tech>
2024-10-03 14:36:06 +0200

    neonvmd: expose interface to control online/offline state of the CPUs
    Signed-off-by: Misha Sakhnov <misha@neon.tech>

--- neonvm-daemon/cmd/main.go
+++ neonvm-daemon/cmd/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
+	"sync"
 	"time"
 
+	"github.com/neondatabase/autoscaling/neonvm/daemon/pkg/cpuscaling"
 	"go.uber.org/zap"
 )
 
@@ -26,24 +31,92 @@ func main() {
 	defer logger.Sync() //nolint:errcheck // what are we gonna do, log something about it?
 
 	logger.Info("Starting neonvm-daemon", zap.String("addr", *addr))
-
-	srv := cpuServer{}
-	srv.run(logger, *addr)
+	cpuHotPlugController := &cpuscaling.CPUOnlineOffliner{}
+	srv := cpuServer{
+		cpuSystemWideScaler: cpuHotPlugController,
+		logger:              logger.Named("cpu-srv"),
+	}
+	srv.run(*addr)
 }
 
-type cpuServer struct{}
+type cpuServer struct {
+	// Protects CPU operations from concurrent access to prevent multiple ensureOnlineCPUs calls from running concurrently
+	// and ensure that status response is always actual
+	cpuOperationsMutex  *sync.Mutex
+	cpuSystemWideScaler *cpuscaling.CPUOnlineOffliner
+	logger              *zap.Logger
+}
 
-func (s *cpuServer) run(logger *zap.Logger, addr string) {
-	logger = logger.Named("cpu-srv")
+// milliCPU is a type that represents CPU in milli units
+type milliCPU uint64
+
+// milliCPUFromString converts a byte slice to milliCPU
+func milliCPUFromString(s []byte) (milliCPU, error) {
+	cpu, err := strconv.ParseUint(string(s), 10, 32)
+	if err != nil {
+		return 0, err
+	}
+	return milliCPU(cpu), nil
+}
+
+// ToCPU converts milliCPU to CPU
+func (m milliCPU) ToCPU() int {
+	cpu := float64(m) / 1000.0
+	// Use math.Ceil to round up to the next CPU.
+	return int(math.Ceil(cpu))
+}
+
+func (s *cpuServer) handleGetCPUStatus(w http.ResponseWriter) {
+	s.cpuOperationsMutex.Lock()
+	defer s.cpuOperationsMutex.Unlock()
+	totalCPUs, err := s.cpuSystemWideScaler.GetTotalCPUsCount()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	activeCPUs, err := s.cpuSystemWideScaler.GetActiveCPUsCount()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(fmt.Sprintf("%d %d", activeCPUs, totalCPUs)))
+	w.WriteHeader(http.StatusOK)
+}
+
+func (s *cpuServer) handleSetCPUStatus(w http.ResponseWriter, r *http.Request) {
+	s.cpuOperationsMutex.Lock()
+	defer s.cpuOperationsMutex.Unlock()
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		s.logger.Error("could not read request body", zap.Error(err))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	milliCPU, err := milliCPUFromString(body)
+	if err != nil {
+		s.logger.Error("could not parse request body as uint32", zap.Error(err))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if err := s.cpuSystemWideScaler.EnsureOnlineCPUs(milliCPU.ToCPU()); err != nil {
+		s.logger.Error("failed to ensure online CPUs", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (s *cpuServer) run(addr string) {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/cpu", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
-			logger.Error("unimplemented!")
-			w.WriteHeader(http.StatusInternalServerError)
+			s.handleGetCPUStatus(w)
+			return
 		} else if r.Method == http.MethodPut {
-			logger.Error("unimplemented!")
-			w.WriteHeader(http.StatusInternalServerError)
+			s.handleSetCPUStatus(w, r)
+			return
 		} else {
 			// unknown method
 			w.WriteHeader(http.StatusNotFound)
@@ -61,7 +134,7 @@ func (s *cpuServer) run(logger *zap.Logger, addr string) {
 
 	err := server.ListenAndServe()
 	if err != nil {
-		logger.Fatal("CPU server exited with error", zap.Error(err))
+		s.logger.Fatal("CPU server exited with error", zap.Error(err))
 	}
-	logger.Info("CPU server exited without error")
+	s.logger.Info("CPU server exited without error")
 }
