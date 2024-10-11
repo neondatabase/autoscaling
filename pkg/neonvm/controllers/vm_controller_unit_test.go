be2ad48114f3c64931c0ccb10873d6c4ef17c873
Misha Sakhnov <misha@neon.tech>
2024-10-11 12:39:09 +0200

    neonvm-controller: add spec.cpuScalingMode, change vm-controller to explicitly default the field value if it is not set
    Signed-off-by: Misha Sakhnov <misha@neon.tech>

--- pkg/neonvm/controllers/vm_controller_unit_test.go
+++ pkg/neonvm/controllers/vm_controller_unit_test.go
@@ -166,6 +166,11 @@ func TestReconcile(t *testing.T) {
 	}, res)
 	assert.Contains(t, params.getVM().Finalizers, virtualmachineFinalizer)
 
+	// Added default value for the cpuScalingMode
+	res, err = params.r.Reconcile(params.ctx, req)
+	assert.NoError(t, err)
+	assert.Equal(t, true, res.Requeue)
+
 	// Round 2
 	res, err = params.r.Reconcile(params.ctx, req)
 	assert.NoError(t, err)
@@ -184,8 +189,11 @@ func TestReconcile(t *testing.T) {
 	// We now have a pod
 	vm := params.getVM()
 	assert.NotEmpty(t, vm.Status.PodName)
-	// Spec is unchanged
-	assert.Equal(t, vm.Spec, origVM.Spec)
+	// Spec is unchanged except cpuScalingMode
+	var origVMWithCPUScalingMode vmv1.VirtualMachine
+	origVM.DeepCopy().DeepCopyInto(&origVMWithCPUScalingMode)
+	origVMWithCPUScalingMode.Spec.CpuScalingMode = lo.ToPtr(vmv1.CpuScalingModeQMP)
+	assert.Equal(t, vm.Spec, origVMWithCPUScalingMode.Spec)
 
 	// Round 4
 	res, err = params.r.Reconcile(params.ctx, req)
@@ -216,7 +224,12 @@ func TestRunningPod(t *testing.T) {
 	// Round 1
 	params.mockRecorder.On("Event", mock.Anything, "Normal", "Created",
 		mock.Anything)
+	// Requeue because we expect default values to be set
 	res, err := params.r.Reconcile(params.ctx, req)
+	require.NoError(t, err)
+	assert.Equal(t, true, res.Requeue)
+
+	res, err = params.r.Reconcile(params.ctx, req)
 	require.NoError(t, err)
 	assert.Equal(t, false, res.Requeue)
 
