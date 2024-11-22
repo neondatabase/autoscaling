3e674b58c79f85185dcba791996cf84346d10350
Em Sharnoff <sharnoff@neon.tech>
2024-11-22 15:28:33 -0800

    neonvm: Add helpers to get pod ownership
    We had some existing, similar helpers in 'pkg/util', but I figured it
    makes more sense for those to be defined in 'neonvm/apis/...'.
    
    This change originated while working on the stateless scheduler
    prototype, where I found myself wanting a reliable way to determine what
    role a pod has in a live migration from only the pod's metadata.

--- pkg/util/k8s.go
+++ pkg/util/k8s.go
@@ -3,9 +3,9 @@ package util
 // Kubernetes-specific utility functions
 
 import (
-	"strings"
-
 	corev1 "k8s.io/api/core/v1"
+
+	vmv1 "github.com/neondatabase/autoscaling/neonvm/apis/neonvm/v1"
 )
 
 // PodReady returns true iff the pod is marked as ready (as determined by the pod's
@@ -74,35 +74,24 @@ func PodPreferredAZIfPresent(pod *corev1.Pod) string {
 // TryPodOwnerVirtualMachine returns the name of the VirtualMachine that owns the pod, if there is
 // one that does. Otherwise returns nil.
 func TryPodOwnerVirtualMachine(pod *corev1.Pod) *NamespacedName {
-	for _, ref := range pod.OwnerReferences {
-		// For NeonVM, *at time of writing*, the OwnerReference has an APIVersion of
-		// "vm.neon.tech/v1". But:
-		//
-		// 1. It's good to be extra-safe around possible name collisions for the
-		//    "VirtualMachineMigration" name, even though *practically* it's not going to happen;
-		// 2. We can disambiguate with the APIVersion; and
-		// 3. We don't want to match on a fixed version, in case we want to change the version
-		//    number later.
-		//
-		// So, given that the format is "<NAME>/<VERSION>", we can just match on the "<NAME>/" part
-		// of the APIVersion to have the safety we want with the flexibility we need.
-		if strings.HasPrefix(ref.APIVersion, "vm.neon.tech/") && ref.Kind == "VirtualMachine" {
-			// note: OwnerReferences are not permitted to have a different namespace than the owned
-			// object, so because VirtualMachineMigrations are namespaced, it must have the same
-			// namespace as the Pod.
-			return &NamespacedName{Namespace: pod.Namespace, Name: ref.Name}
-		}
+	ref, ok := vmv1.VirtualMachineOwnerForPod(pod)
+	if !ok {
+		return nil
 	}
-	return nil
+
+	// note: OwnerReferences are not permitted to have a different namespace than the owned
+	// object, so because VirtualMachineMigrations are namespaced, it must have the same
+	// namespace as the Pod.
+	return &NamespacedName{Namespace: pod.Namespace, Name: ref.Name}
 }
 
 // TryPodOwnerVirtualMachineMigration returns the name of the VirtualMachineMigration that owns the
 // pod, if there is one. Otherwise returns nil.
 func TryPodOwnerVirtualMachineMigration(pod *corev1.Pod) *NamespacedName {
-	for _, ref := range pod.OwnerReferences {
-		if strings.HasPrefix(ref.APIVersion, "vm.neon.tech/") && ref.Kind == "VirtualMachineMigration" {
-			return &NamespacedName{Namespace: pod.Namespace, Name: ref.Name}
-		}
+	ref, _, ok := vmv1.MigrationOwnerForPod(pod)
+	if !ok {
+		return nil
 	}
-	return nil
+
+	return &NamespacedName{Namespace: pod.Namespace, Name: ref.Name}
 }
