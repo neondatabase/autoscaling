f830fbfa807298a20e6558889f5ad85ffc8b29e3
Em Sharnoff <sharnoff@neon.tech>
2024-12-23 13:21:56 -0800

    agent: Add support for simplified LFC scaling
    We have one ongoing issue where the LFC goal size algorithm is hitting
    degenerate edge-case behavior on a large workload -- but because of that
    bug, it's hard to tell whether the impacted workload *can* be feasibly
    autoscaled with the information we have available.
    
    For large workloads, it's reasonable enough to just look at the 1-hour
    working set window, and testing with that will indicate whether we need
    to look at a longer time horizon to effectively scale LFC on such a
    workload -- or if the information we already have should be sufficient.
    
    See also neondatabase/cloud#22214 for more information.

--- pkg/api/vminfo.go
+++ pkg/api/vminfo.go
@@ -353,6 +353,13 @@ type ScalingConfig struct {
 	// default.
 	EnableLFCMetrics *bool `json:"enableLFCMetrics,omitempty"`
 
+	// UseSimpleLFCScaling, if true, calculates goal LFC size only based on the largest available
+	// working set size window, instead of trying to allow downscaling at earlier opportunities.
+	//
+	// This is not fit for general use. It's meant as a temproary escape hatch to let us assess the
+	// upper bound of potential improvements to our LFC goal size heuristics.
+	UseSimpleLFCScaling *bool `json:"useSimpleLFCScaling,omitempty"`
+
 	// LFCToMemoryRatio dictates the amount of memory in any given Compute Unit that will be
 	// allocated to the LFC. For example, if the LFC is sized at 75% of memory, then this value
 	// would be 0.75.
@@ -400,6 +407,9 @@ func (defaults ScalingConfig) WithOverrides(overrides *ScalingConfig) ScalingCon
 	}
 	if overrides.EnableLFCMetrics != nil {
 		defaults.EnableLFCMetrics = lo.ToPtr(*overrides.EnableLFCMetrics)
+	}
+	if overrides.UseSimpleLFCScaling != nil {
+		defaults.UseSimpleLFCScaling = lo.ToPtr(*overrides.UseSimpleLFCScaling)
 	}
 	if overrides.LFCToMemoryRatio != nil {
 		defaults.LFCToMemoryRatio = lo.ToPtr(*overrides.LFCToMemoryRatio)
