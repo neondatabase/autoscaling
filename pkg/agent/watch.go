4a81fb925af211330c7c05f89da4f1aad3dd4379
Oleg Vasilev <oleg@neon.tech>
2025-03-02 22:34:55 +0000

    agent: eliminate repetition in per-VM metrics
    Signed-off-by: Oleg Vasilev <oleg@neon.tech>

--- pkg/agent/watch.go
+++ pkg/agent/watch.go
@@ -300,24 +300,45 @@ func makeVMRestartMetrics(vm *vmv1.VirtualMachine) []vmMetric {
 	}
 }
 
+// gaugeSpec groups a source of metrics (maker) with a destination (gauge).
+type gaugeSpec struct {
+	maker func(*vmv1.VirtualMachine) []vmMetric
+	gauge *prometheus.GaugeVec
+}
+
+// getGaugeSpecs constructs our list of metrics to expose.
+//
+// It is used for creating, updating, and deleting metrics.
+func getGaugeSpecs(perVMMetrics *PerVMMetrics) []gaugeSpec {
+	return []gaugeSpec{
+		{
+			maker: makeVMCPUMetrics,
+			gauge: perVMMetrics.cpu,
+		},
+		{
+			maker: makeVMMemMetrics,
+			gauge: perVMMetrics.memory,
+		},
+		{
+			maker: makeVMRestartMetrics,
+			gauge: perVMMetrics.restartCount,
+		},
+	}
+}
+
 func setVMMetrics(perVMMetrics *PerVMMetrics, vm *vmv1.VirtualMachine, nodeName string) {
 	if vm.Status.Node != nodeName {
 		return
 	}
 
-	cpuMetrics := makeVMCPUMetrics(vm)
-	for _, m := range cpuMetrics {
-		perVMMetrics.cpu.With(m.labels).Set(m.value)
+	push := func(metrics []vmMetric, gauge *prometheus.GaugeVec) {
+		for _, m := range metrics {
+			gauge.With(m.labels).Set(m.value)
+		}
 	}
 
-	memMetrics := makeVMMemMetrics(vm)
-	for _, m := range memMetrics {
-		perVMMetrics.memory.With(m.labels).Set(m.value)
-	}
-
-	restartCountMetrics := makeVMRestartMetrics(vm)
-	for _, m := range restartCountMetrics {
-		perVMMetrics.restartCount.With(m.labels).Set(m.value)
+	for _, spec := range getGaugeSpecs(perVMMetrics) {
+		push(spec.maker(vm), spec.gauge)
 	}
 
 	// Add the VM to the internal tracker:
@@ -349,17 +370,11 @@ func updateVMMetrics(perVMMetrics *PerVMMetrics, oldVM, newVM *vmv1.VirtualMachi
 		}
 	}
 
-	oldCPUMetrics := makeVMCPUMetrics(oldVM)
-	newCPUMetrics := makeVMCPUMetrics(newVM)
-	updateMetrics(perVMMetrics.cpu, oldCPUMetrics, newCPUMetrics)
-
-	oldMemMetrics := makeVMMemMetrics(oldVM)
-	newMemMetrics := makeVMMemMetrics(newVM)
-	updateMetrics(perVMMetrics.memory, oldMemMetrics, newMemMetrics)
-
-	oldRestartCountMetrics := makeVMRestartMetrics(oldVM)
-	newRestartCountMetrics := makeVMRestartMetrics(newVM)
-	updateMetrics(perVMMetrics.restartCount, oldRestartCountMetrics, newRestartCountMetrics)
+	for _, spec := range getGaugeSpecs(perVMMetrics) {
+		oldMetrics := spec.maker(oldVM)
+		newMetrics := spec.maker(newVM)
+		updateMetrics(spec.gauge, oldMetrics, newMetrics)
+	}
 
 	// Update the VM in the internal tracker:
 	perVMMetrics.updateActive(newVM) // note: don't need to clean up old one, because it's keyed by name
@@ -370,19 +385,11 @@ func deleteVMMetrics(perVMMetrics *PerVMMetrics, vm *vmv1.VirtualMachine, nodeNa
 		return
 	}
 
-	cpuMetrics := makeVMCPUMetrics(vm)
-	for _, m := range cpuMetrics {
-		perVMMetrics.cpu.Delete(m.labels)
-	}
-
-	memMetrics := makeVMMemMetrics(vm)
-	for _, m := range memMetrics {
-		perVMMetrics.memory.Delete(m.labels)
-	}
-
-	restartCountMetrics := makeVMRestartMetrics(vm)
-	for _, m := range restartCountMetrics {
-		perVMMetrics.restartCount.Delete(m.labels)
+	for _, spec := range getGaugeSpecs(perVMMetrics) {
+		metrics := spec.maker(vm)
+		for _, m := range metrics {
+			spec.gauge.Delete(m.labels)
+		}
 	}
 
 	// Remove the VM from the internal tracker:
