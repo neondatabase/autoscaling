5e6ed57fafb46048676b70687e25fd5035af33c5
Em Sharnoff <sharnoff@neon.tech>
2025-04-09 02:23:21 -0700

    Update to kubernetes 1.31 (#1322)
    Closes #1264; see there for more high-level context.

--- neonvm/client/clientset/versioned/typed/neonvm/v1/ippool.go
+++ neonvm/client/clientset/versioned/typed/neonvm/v1/ippool.go
@@ -19,14 +19,13 @@ package v1
 
 import (
 	"context"
-	"time"
 
 	v1 "github.com/neondatabase/autoscaling/neonvm/apis/neonvm/v1"
 	scheme "github.com/neondatabase/autoscaling/neonvm/client/clientset/versioned/scheme"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
-	rest "k8s.io/client-go/rest"
+	gentype "k8s.io/client-go/gentype"
 )
 
 // IPPoolsGetter has a method to return a IPPoolInterface.
@@ -50,128 +49,18 @@ type IPPoolInterface interface {
 
 // iPPools implements IPPoolInterface
 type iPPools struct {
-	client rest.Interface
-	ns     string
+	*gentype.ClientWithList[*v1.IPPool, *v1.IPPoolList]
 }
 
 // newIPPools returns a IPPools
 func newIPPools(c *NeonvmV1Client, namespace string) *iPPools {
 	return &iPPools{
-		client: c.RESTClient(),
-		ns:     namespace,
+		gentype.NewClientWithList[*v1.IPPool, *v1.IPPoolList](
+			"ippools",
+			c.RESTClient(),
+			scheme.ParameterCodec,
+			namespace,
+			func() *v1.IPPool { return &v1.IPPool{} },
+			func() *v1.IPPoolList { return &v1.IPPoolList{} }),
 	}
-}
-
-// Get takes name of the iPPool, and returns the corresponding iPPool object, and an error if there is any.
-func (c *iPPools) Get(ctx context.Context, name string, options metav1.GetOptions) (result *v1.IPPool, err error) {
-	result = &v1.IPPool{}
-	err = c.client.Get().
-		Namespace(c.ns).
-		Resource("ippools").
-		Name(name).
-		VersionedParams(&options, scheme.ParameterCodec).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// List takes label and field selectors, and returns the list of IPPools that match those selectors.
-func (c *iPPools) List(ctx context.Context, opts metav1.ListOptions) (result *v1.IPPoolList, err error) {
-	var timeout time.Duration
-	if opts.TimeoutSeconds != nil {
-		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
-	}
-	result = &v1.IPPoolList{}
-	err = c.client.Get().
-		Namespace(c.ns).
-		Resource("ippools").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Watch returns a watch.Interface that watches the requested iPPools.
-func (c *iPPools) Watch(ctx context.Context, opts metav1.ListOptions) (watch.Interface, error) {
-	var timeout time.Duration
-	if opts.TimeoutSeconds != nil {
-		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
-	}
-	opts.Watch = true
-	return c.client.Get().
-		Namespace(c.ns).
-		Resource("ippools").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Watch(ctx)
-}
-
-// Create takes the representation of a iPPool and creates it.  Returns the server's representation of the iPPool, and an error, if there is any.
-func (c *iPPools) Create(ctx context.Context, iPPool *v1.IPPool, opts metav1.CreateOptions) (result *v1.IPPool, err error) {
-	result = &v1.IPPool{}
-	err = c.client.Post().
-		Namespace(c.ns).
-		Resource("ippools").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(iPPool).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Update takes the representation of a iPPool and updates it. Returns the server's representation of the iPPool, and an error, if there is any.
-func (c *iPPools) Update(ctx context.Context, iPPool *v1.IPPool, opts metav1.UpdateOptions) (result *v1.IPPool, err error) {
-	result = &v1.IPPool{}
-	err = c.client.Put().
-		Namespace(c.ns).
-		Resource("ippools").
-		Name(iPPool.Name).
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(iPPool).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Delete takes name of the iPPool and deletes it. Returns an error if one occurs.
-func (c *iPPools) Delete(ctx context.Context, name string, opts metav1.DeleteOptions) error {
-	return c.client.Delete().
-		Namespace(c.ns).
-		Resource("ippools").
-		Name(name).
-		Body(&opts).
-		Do(ctx).
-		Error()
-}
-
-// DeleteCollection deletes a collection of objects.
-func (c *iPPools) DeleteCollection(ctx context.Context, opts metav1.DeleteOptions, listOpts metav1.ListOptions) error {
-	var timeout time.Duration
-	if listOpts.TimeoutSeconds != nil {
-		timeout = time.Duration(*listOpts.TimeoutSeconds) * time.Second
-	}
-	return c.client.Delete().
-		Namespace(c.ns).
-		Resource("ippools").
-		VersionedParams(&listOpts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Body(&opts).
-		Do(ctx).
-		Error()
-}
-
-// Patch applies the patch and returns the patched iPPool.
-func (c *iPPools) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts metav1.PatchOptions, subresources ...string) (result *v1.IPPool, err error) {
-	result = &v1.IPPool{}
-	err = c.client.Patch(pt).
-		Namespace(c.ns).
-		Resource("ippools").
-		Name(name).
-		SubResource(subresources...).
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(data).
-		Do(ctx).
-		Into(result)
-	return
 }
--- neonvm/client/clientset/versioned/typed/neonvm/v1/virtualmachinemigration.go
+++ neonvm/client/clientset/versioned/typed/neonvm/v1/virtualmachinemigration.go
@@ -19,14 +19,13 @@ package v1
 
 import (
 	"context"
-	"time"
 
 	v1 "github.com/neondatabase/autoscaling/neonvm/apis/neonvm/v1"
 	scheme "github.com/neondatabase/autoscaling/neonvm/client/clientset/versioned/scheme"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
-	rest "k8s.io/client-go/rest"
+	gentype "k8s.io/client-go/gentype"
 )
 
 // VirtualMachineMigrationsGetter has a method to return a VirtualMachineMigrationInterface.
@@ -39,6 +38,7 @@ type VirtualMachineMigrationsGetter interface {
 type VirtualMachineMigrationInterface interface {
 	Create(ctx context.Context, virtualMachineMigration *v1.VirtualMachineMigration, opts metav1.CreateOptions) (*v1.VirtualMachineMigration, error)
 	Update(ctx context.Context, virtualMachineMigration *v1.VirtualMachineMigration, opts metav1.UpdateOptions) (*v1.VirtualMachineMigration, error)
+	// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
 	UpdateStatus(ctx context.Context, virtualMachineMigration *v1.VirtualMachineMigration, opts metav1.UpdateOptions) (*v1.VirtualMachineMigration, error)
 	Delete(ctx context.Context, name string, opts metav1.DeleteOptions) error
 	DeleteCollection(ctx context.Context, opts metav1.DeleteOptions, listOpts metav1.ListOptions) error
@@ -51,144 +51,18 @@ type VirtualMachineMigrationInterface interface {
 
 // virtualMachineMigrations implements VirtualMachineMigrationInterface
 type virtualMachineMigrations struct {
-	client rest.Interface
-	ns     string
+	*gentype.ClientWithList[*v1.VirtualMachineMigration, *v1.VirtualMachineMigrationList]
 }
 
 // newVirtualMachineMigrations returns a VirtualMachineMigrations
 func newVirtualMachineMigrations(c *NeonvmV1Client, namespace string) *virtualMachineMigrations {
 	return &virtualMachineMigrations{
-		client: c.RESTClient(),
-		ns:     namespace,
+		gentype.NewClientWithList[*v1.VirtualMachineMigration, *v1.VirtualMachineMigrationList](
+			"virtualmachinemigrations",
+			c.RESTClient(),
+			scheme.ParameterCodec,
+			namespace,
+			func() *v1.VirtualMachineMigration { return &v1.VirtualMachineMigration{} },
+			func() *v1.VirtualMachineMigrationList { return &v1.VirtualMachineMigrationList{} }),
 	}
-}
-
-// Get takes name of the virtualMachineMigration, and returns the corresponding virtualMachineMigration object, and an error if there is any.
-func (c *virtualMachineMigrations) Get(ctx context.Context, name string, options metav1.GetOptions) (result *v1.VirtualMachineMigration, err error) {
-	result = &v1.VirtualMachineMigration{}
-	err = c.client.Get().
-		Namespace(c.ns).
-		Resource("virtualmachinemigrations").
-		Name(name).
-		VersionedParams(&options, scheme.ParameterCodec).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// List takes label and field selectors, and returns the list of VirtualMachineMigrations that match those selectors.
-func (c *virtualMachineMigrations) List(ctx context.Context, opts metav1.ListOptions) (result *v1.VirtualMachineMigrationList, err error) {
-	var timeout time.Duration
-	if opts.TimeoutSeconds != nil {
-		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
-	}
-	result = &v1.VirtualMachineMigrationList{}
-	err = c.client.Get().
-		Namespace(c.ns).
-		Resource("virtualmachinemigrations").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Watch returns a watch.Interface that watches the requested virtualMachineMigrations.
-func (c *virtualMachineMigrations) Watch(ctx context.Context, opts metav1.ListOptions) (watch.Interface, error) {
-	var timeout time.Duration
-	if opts.TimeoutSeconds != nil {
-		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
-	}
-	opts.Watch = true
-	return c.client.Get().
-		Namespace(c.ns).
-		Resource("virtualmachinemigrations").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Watch(ctx)
-}
-
-// Create takes the representation of a virtualMachineMigration and creates it.  Returns the server's representation of the virtualMachineMigration, and an error, if there is any.
-func (c *virtualMachineMigrations) Create(ctx context.Context, virtualMachineMigration *v1.VirtualMachineMigration, opts metav1.CreateOptions) (result *v1.VirtualMachineMigration, err error) {
-	result = &v1.VirtualMachineMigration{}
-	err = c.client.Post().
-		Namespace(c.ns).
-		Resource("virtualmachinemigrations").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(virtualMachineMigration).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Update takes the representation of a virtualMachineMigration and updates it. Returns the server's representation of the virtualMachineMigration, and an error, if there is any.
-func (c *virtualMachineMigrations) Update(ctx context.Context, virtualMachineMigration *v1.VirtualMachineMigration, opts metav1.UpdateOptions) (result *v1.VirtualMachineMigration, err error) {
-	result = &v1.VirtualMachineMigration{}
-	err = c.client.Put().
-		Namespace(c.ns).
-		Resource("virtualmachinemigrations").
-		Name(virtualMachineMigration.Name).
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(virtualMachineMigration).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// UpdateStatus was generated because the type contains a Status member.
-// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
-func (c *virtualMachineMigrations) UpdateStatus(ctx context.Context, virtualMachineMigration *v1.VirtualMachineMigration, opts metav1.UpdateOptions) (result *v1.VirtualMachineMigration, err error) {
-	result = &v1.VirtualMachineMigration{}
-	err = c.client.Put().
-		Namespace(c.ns).
-		Resource("virtualmachinemigrations").
-		Name(virtualMachineMigration.Name).
-		SubResource("status").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(virtualMachineMigration).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Delete takes name of the virtualMachineMigration and deletes it. Returns an error if one occurs.
-func (c *virtualMachineMigrations) Delete(ctx context.Context, name string, opts metav1.DeleteOptions) error {
-	return c.client.Delete().
-		Namespace(c.ns).
-		Resource("virtualmachinemigrations").
-		Name(name).
-		Body(&opts).
-		Do(ctx).
-		Error()
-}
-
-// DeleteCollection deletes a collection of objects.
-func (c *virtualMachineMigrations) DeleteCollection(ctx context.Context, opts metav1.DeleteOptions, listOpts metav1.ListOptions) error {
-	var timeout time.Duration
-	if listOpts.TimeoutSeconds != nil {
-		timeout = time.Duration(*listOpts.TimeoutSeconds) * time.Second
-	}
-	return c.client.Delete().
-		Namespace(c.ns).
-		Resource("virtualmachinemigrations").
-		VersionedParams(&listOpts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Body(&opts).
-		Do(ctx).
-		Error()
-}
-
-// Patch applies the patch and returns the patched virtualMachineMigration.
-func (c *virtualMachineMigrations) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts metav1.PatchOptions, subresources ...string) (result *v1.VirtualMachineMigration, err error) {
-	result = &v1.VirtualMachineMigration{}
-	err = c.client.Patch(pt).
-		Namespace(c.ns).
-		Resource("virtualmachinemigrations").
-		Name(name).
-		SubResource(subresources...).
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(data).
-		Do(ctx).
-		Into(result)
-	return
 }
