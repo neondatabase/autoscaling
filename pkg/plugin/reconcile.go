deb0a433c0fbeedacabeef8138cedcdc9bb80a31
Em Sharnoff <sharnoff@neon.tech>
2025-04-16 18:31:38 +0100

    plugin: Move reconcile queue callbacks to metrics
    This removes a layer of indirection via the plugin methods, which means
    we no longer have to require that the plugin is initialized before any
    of the methods are called.

--- pkg/plugin/reconcile.go
+++ pkg/plugin/reconcile.go
@@ -3,46 +3,27 @@ package plugin
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"go.uber.org/zap"
 
 	"github.com/neondatabase/autoscaling/pkg/plugin/reconcile"
 )
 
-func (s *PluginState) reconcileQueueWaitCallback(duration time.Duration) {
-	s.metrics.Reconcile.WaitDurations.Observe(duration.Seconds())
-}
-
-func (s *PluginState) reconcileResultCallback(params reconcile.ObjectParams, duration time.Duration, err error) {
-	outcome := "success"
-	if err != nil {
-		outcome = "failure"
-	}
-	s.metrics.Reconcile.ProcessDurations.
-		WithLabelValues(params.GVK.Kind, outcome).
-		Observe(duration.Seconds())
-}
-
-func (s *PluginState) reconcileErrorStatsCallback(logger *zap.Logger, params reconcile.ObjectParams, stats reconcile.ErrorStats) {
-	// update count of current failing objects
-	s.metrics.Reconcile.Failing.
-		WithLabelValues(params.GVK.Kind).
-		Set(float64(stats.TypedCount))
-
+func logSuccessiveReconcileFailure(
+	logger *zap.Logger,
+	params reconcile.ObjectParams,
+	stats reconcile.ErrorStats,
+	threshold int,
+) {
 	// Make sure that repeatedly failing objects are sufficiently noisy
-	if stats.SuccessiveFailures >= s.config.LogSuccessiveFailuresThreshold {
+	if stats.SuccessiveFailures >= threshold {
 		logger.Warn(
-			fmt.Sprintf("%s has failed to reconcile >%d times in a row", params.GVK.Kind, s.config.LogSuccessiveFailuresThreshold),
+			fmt.Sprintf("%s has failed to reconcile >%d times in a row", params.GVK.Kind, threshold),
 			zap.Int("SuccessiveFailures", stats.SuccessiveFailures),
 			zap.String("EventKind", string(params.EventKind)),
 			reconcile.ObjectMetaLogField(params.GVK.Kind, params.Obj),
 		)
 	}
-}
-
-func (s *PluginState) reconcilePanicCallback(params reconcile.ObjectParams) {
-	s.metrics.Reconcile.Panics.WithLabelValues(params.GVK.Kind).Inc()
 }
 
 func reconcileWorker(ctx context.Context, logger *zap.Logger, queue *reconcile.Queue) {
