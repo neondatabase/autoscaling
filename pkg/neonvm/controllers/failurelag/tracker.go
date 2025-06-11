e5284fc593eaa59699e10747d6052874d406b72a
Oleg Vasilev <oleg@neon.tech>
2025-06-11 16:07:07 +0000

    neonvm-controller: adjust the semantics (#1398)
    No functional changes, preparation for the next patch.

--- pkg/neonvm/controllers/failurelag/tracker.go
+++ pkg/neonvm/controllers/failurelag/tracker.go
@@ -12,52 +12,56 @@ import (
 type Tracker[T comparable] struct {
 	period time.Duration
 
-	pendingSince map[T]time.Time
-	degraded     map[T]struct{}
-	degradeAt    []degradeAt[T]
+	firstFailure map[T]time.Time
+	refreshQueue []refreshAt[T]
+
+	degraded map[T]struct{}
 
 	lock sync.Mutex
 	Now  func() time.Time
 }
 
-type degradeAt[T comparable] struct {
+type refreshAt[T comparable] struct {
 	ts  time.Time
 	key T
 }
 
 func NewTracker[T comparable](period time.Duration) *Tracker[T] {
 	return &Tracker[T]{
-		period:       period,
-		pendingSince: make(map[T]time.Time),
-		degraded:     make(map[T]struct{}),
-		degradeAt:    []degradeAt[T]{},
-		lock:         sync.Mutex{},
-		Now:          time.Now,
+		period: period,
+
+		firstFailure: make(map[T]time.Time),
+		refreshQueue: make([]refreshAt[T], 0),
+
+		degraded: make(map[T]struct{}),
+
+		lock: sync.Mutex{},
+		Now:  time.Now,
 	}
 }
 
-// forward processes all the fireAt events that are now in the past.
-func (t *Tracker[T]) forward(now time.Time) {
+// processQueue processes all refresh requests that are now in the past.
+func (t *Tracker[T]) processQueue(now time.Time) {
 	i := 0
-	for ; i < len(t.degradeAt); i++ {
-		event := t.degradeAt[i]
+	for ; i < len(t.refreshQueue); i++ {
+		event := t.refreshQueue[i]
 		if event.ts.After(now) {
 			break
 		}
-		pendingSince, ok := t.pendingSince[event.key]
+		firstFailure, ok := t.firstFailure[event.key]
 		if !ok {
 			// There was a success event in between
 			continue
 		}
 
-		if event.ts.Sub(pendingSince) < t.period {
+		if event.ts.Sub(firstFailure) < t.period {
 			// There was a success, and another failure in between
 			// We will have another fireAt event for this key in the future
 			continue
 		}
 		t.degraded[event.key] = struct{}{}
 	}
-	t.degradeAt = t.degradeAt[i:]
+	t.refreshQueue = t.refreshQueue[i:]
 }
 
 func (t *Tracker[T]) RecordSuccess(key T) {
@@ -65,8 +69,8 @@ func (t *Tracker[T]) RecordSuccess(key T) {
 	defer t.lock.Unlock()
 
 	delete(t.degraded, key)
-	delete(t.pendingSince, key)
-	t.forward(t.Now())
+	delete(t.firstFailure, key)
+	t.processQueue(t.Now())
 }
 
 func (t *Tracker[T]) RecordFailure(key T) {
@@ -75,23 +79,23 @@ func (t *Tracker[T]) RecordFailure(key T) {
 
 	now := t.Now()
 
-	if _, ok := t.pendingSince[key]; !ok {
-		t.pendingSince[key] = now
+	if _, ok := t.firstFailure[key]; !ok {
+		t.firstFailure[key] = now
 	}
 
-	t.degradeAt = append(t.degradeAt, degradeAt[T]{
+	t.refreshQueue = append(t.refreshQueue, refreshAt[T]{
 		ts:  now.Add(t.period),
 		key: key,
 	})
 
-	t.forward(now)
+	t.processQueue(now)
 }
 
 func (t *Tracker[T]) DegradedCount() int {
 	t.lock.Lock()
 	defer t.lock.Unlock()
 
-	t.forward(t.Now())
+	t.processQueue(t.Now())
 	return len(t.degraded)
 }
 
@@ -99,7 +103,7 @@ func (t *Tracker[T]) Degraded() []T {
 	t.lock.Lock()
 	defer t.lock.Unlock()
 
-	t.forward(t.Now())
+	t.processQueue(t.Now())
 	keys := make([]T, 0, len(t.degraded))
 	for k := range t.degraded {
 		keys = append(keys, k)
